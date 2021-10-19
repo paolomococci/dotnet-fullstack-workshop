bcec2ea9ae76908dba358a6d773b322d440079e6
paolomococci <paolomococci@gmail.com>
2021-10-19 16:33:57 +0200

    I added class JourneySelection to project Journey

---
+++ Journey/src/core/Journey.Domain/Entities/JourneySelection.cs
@@ -0,0 +1,14 @@
+namespace Journey.Domain.Entities
+{
+    public class JourneySelection
+    {
+        public int Id { get; set; }
+        public int ListId { get; set; }
+        public string Name { get; set; }
+        public string Hope { get; set; }
+        public string MapLocation { get; set; }
+        public float Price { get; set; }
+        public int Duration { get; set; }
+        public bool Confirmation { get; set; }
+    }
+}
