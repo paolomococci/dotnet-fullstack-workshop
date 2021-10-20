38904d5fe0126c7657c0b50c0e199a030ff642e0
paolomococci <paolomococci@gmail.com>
2021-10-20 07:34:06 +0200

    I added the get method to both controllers

--- Journey/src/presentation/Journey.WebApi/Controllers/JourneySelectionController.cs
+++ Journey/src/presentation/Journey.WebApi/Controllers/JourneySelectionController.cs
@@ -2,11 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Journey.Data.Contexts;
+using Journey.Domain.Entities;
 
 namespace Journey.WebApi.Controllers
 {
-    public class JourneySelectionController
+    [ApiController]
+    [Route("api/[controller")]
+    public class JourneySelectionController : ControllerBase
     {
-        
+        private readonly JourneyDbContext _journeyDbContext;
+
+        public JourneySelectionController(JourneyDbContext journeyDbContext)
+        {
+            _journeyDbContext = journeyDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetActionResult()
+        {
+            return Ok(_journeyDbContext.JourneySelections);
+        }
     }
 }
