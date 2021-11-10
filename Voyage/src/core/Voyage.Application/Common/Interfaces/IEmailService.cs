b07af8cacb5481e0a5bf920e6c985a2d8aac3ec8
paolomococci <paolomococci@gmail.com>
2021-11-10 15:15:26 +0100

    I went back to editing interface IEmailService of project core of solution Voyage

--- Voyage/src/core/Voyage.Application/Common/Interfaces/IEmailService.cs
+++ Voyage/src/core/Voyage.Application/Common/Interfaces/IEmailService.cs
@@ -1,8 +1,10 @@
+using System.Threading.Tasks;
+using Voyage.Application.Dtos.Email;
+
 namespace Voyage.Application.Common.Interfaces
 {
     public interface IEmailService
     {
-
+        Task SendAsync(EmailDto emailRequest);
     }
 }
-
