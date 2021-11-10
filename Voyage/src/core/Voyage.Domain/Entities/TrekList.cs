43ac016858cf4f81cb1449200884adc71459f8f7
paolomococci <paolomococci@gmail.com>
2021-11-10 18:08:10 +0100

    I have edited the class TrekList of the project core of the solution Voyage

--- Voyage/src/core/Voyage.Domain/Entities/TrekList.cs
+++ Voyage/src/core/Voyage.Domain/Entities/TrekList.cs
@@ -4,6 +4,14 @@ namespace Voyage.Domain.Entities
 {
     public class TrekList
     {
+        public TrekList()
+        {
+            TrekPackages = new List<TrekPackage>();
+        }
 
+        public IList<TrekPackage> TrekPackages { get; set; }
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
     }
 }
