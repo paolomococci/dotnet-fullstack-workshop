671bc680b722fea22f03b4448c7cbfe5af88c888
paolomococci <paolomococci@gmail.com>
2021-11-10 18:02:55 +0100

    I have edited the class TrekPackage of the project core of the solution Voyage

--- Voyage/src/core/Voyage.Domain/Entities/TrekPackage.cs
+++ Voyage/src/core/Voyage.Domain/Entities/TrekPackage.cs
@@ -1,12 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Voyage.Domain.Enums;
 
 namespace Voyage.Domain.Entities
 {
     public class TrekPackage
     {
-
+        public int Id { get; set; }
+        public int ListId { get; set; }
+        public string Name { get; set; }
+        public string Hope { get; set; }
+        public string MapLocation { get; set; }
+        public float Price { get; set; }
+        public int Duration { get; set; }
+        public bool Confirmation { get; set; }
+        public Currency Currency { get; set; }
+        public TrekList trekList { get; set; }
     }
 }
