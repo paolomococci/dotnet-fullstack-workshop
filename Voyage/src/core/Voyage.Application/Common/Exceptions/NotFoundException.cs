466325c312bf90291a3eb47413fd4b3be91e4115
paolomococci <paolomococci@gmail.com>
2021-11-10 09:09:48 +0100

    I have edited the class NotFoundException of the project core of the solution Voyage

--- Voyage/src/core/Voyage.Application/Common/Exceptions/NotFoundException.cs
+++ Voyage/src/core/Voyage.Application/Common/Exceptions/NotFoundException.cs
@@ -1,8 +1,17 @@
+using System;
+
 namespace Voyage.Application.Common.Exceptions
 {
-	public class NotFoundException
-	{
-		
-	}
-}
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message) { }
 
+        public NotFoundException(
+            string message, Exception exception
+        ) : base(message, exception) { }
+
+        public NotFoundException(
+            string entityName, object objectKey
+        ) : base($"Entity \"{entityName}\" ({objectKey}) was not found.") { }
+    }
+}
