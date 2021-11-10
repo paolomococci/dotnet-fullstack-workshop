35c9df04e5cd5389a502a04264c2371b13b424c4
paolomococci <paolomococci@gmail.com>
2021-11-10 18:21:25 +0100

    I have edited the interface IApplicationDbContext of the project core of the solution Voyage

--- Voyage/src/core/Voyage.Application/Common/Interfaces/IApplicationDbContext.cs
+++ Voyage/src/core/Voyage.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -1,10 +1,17 @@
 using System.Threading;
 using System.Threading.Tasks;
+using Voyage.Domain.Entities;
 
 namespace Voyage.Application.Common.Interfaces
 {
     public interface IApplicationDbContext
     {
+        /* TODO */
 
+        //DbSet<TrekList> TrekLists { get; set; }
+
+        //DbSet<TrekPackage> TrekPackages { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
     }
 }
