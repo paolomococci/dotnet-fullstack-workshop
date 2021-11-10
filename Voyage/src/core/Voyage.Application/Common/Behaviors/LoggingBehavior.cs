16e99d0154d5a320275603af629933286ecd9f2f
paolomococci <paolomococci@gmail.com>
2021-11-10 07:39:11 +0100

    I have edited the class LoggingBehavior of the project core of the solution Voyage

--- Voyage/src/core/Voyage.Application/Common/Behaviors/LoggingBehavior.cs
+++ Voyage/src/core/Voyage.Application/Common/Behaviors/LoggingBehavior.cs
@@ -1,8 +1,21 @@
+using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
 namespace Voyage.Application.Common.Behaviors
 {
-	public class LoggingBehavior
-	{
-		
-	}
-}
+    public class LoggingBehavior<TRequest> : IRequestPreProcessor<TRequest>
+    {
+        private readonly ILogger iLogger;
 
+        public async Task Process(TRequest tRequest, CancellationToken cancellationToken)
+        {
+            await Task.Run(() =>
+            {
+                var requestName = typeof(TRequest).Name;
+                iLogger.LogInformation("Voyage Request: {@Request}", requestName, tRequest);
+            });
+        }
+    }
+}
