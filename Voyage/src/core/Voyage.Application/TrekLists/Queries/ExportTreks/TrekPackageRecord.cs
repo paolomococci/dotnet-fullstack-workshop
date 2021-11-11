2f164527dbe4f91a6da4b7ce9eea5f0737e5d0e8
paolomococci <paolomococci@gmail.com>
2021-11-11 05:09:48 +0100

    I have edited the class TrekPackageRecord of the project core of the solution Voyage

--- Voyage/src/core/Voyage.Application/TrekLists/Queries/ExportTreks/TrekPackageRecord.cs
+++ Voyage/src/core/Voyage.Application/TrekLists/Queries/ExportTreks/TrekPackageRecord.cs
@@ -1,7 +1,11 @@
+using Voyage.Application.Common.Mappings;
+using Voyage.Domain.Entities;
+
 namespace Voyage.Application.TrekLists.Queries.ExportTreks
 {
-    public class TrekPackageRecord
+    public class TrekPackageRecord : IMapFrom<TrekPackage>
     {
-
+        public string Name { get; set; }
+        public string MapLocation { get; set; }
     }
 }
