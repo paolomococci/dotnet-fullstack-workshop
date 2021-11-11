d5c3499ed455eca0b5986f1f4e5eaf8637ebadd6
paolomococci <paolomococci@gmail.com>
2021-11-11 04:01:18 +0100

    I have edited the class TrekListDto of the project core of the solution Voyage

--- Voyage/src/core/Voyage.Application/Dtos/Trek/TrekListDto.cs
+++ Voyage/src/core/Voyage.Application/Dtos/Trek/TrekListDto.cs
@@ -1,12 +1,19 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Voyage.Application.Common.Mappings;
+using Voyage.Domain.Entities;
 
 namespace Voyage.Application.Dtos.Trek
 {
-    public class TrekListDto
+    public class TrekListDto : IMapFrom<TrekList>
     {
+        public IList<TrekPackageDto> Items { get; set; }
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
 
+        public TrekListDto()
+        {
+            Items = new List<TrekPackageDto>();
+        }
     }
 }
