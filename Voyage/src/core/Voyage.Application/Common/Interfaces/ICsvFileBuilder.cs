aaa2947ac1f01fc4d225f40bd969f169082cc4c5
paolomococci <paolomococci@gmail.com>
2021-11-11 05:13:04 +0100

    I have edited the interface ICsvFileBuilder of the project core of the solution Voyage

--- Voyage/src/core/Voyage.Application/Common/Interfaces/ICsvFileBuilder.cs
+++ Voyage/src/core/Voyage.Application/Common/Interfaces/ICsvFileBuilder.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
+using Voyage.Application.TrekLists.Queries.ExportTreks;
+
 namespace Voyage.Application.Common.Interfaces
 {
     public interface ICsvFileBuilder
     {
-
+        byte[] BuildTrekPackagesFile(IEnumerable<TrekPackageRecord> records);
     }
 }
