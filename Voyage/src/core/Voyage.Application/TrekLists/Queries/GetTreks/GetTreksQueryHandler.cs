c9e67dde69d0dce825a307e7c63de6ea7b96bb44
paolomococci <paolomococci@gmail.com>
2021-11-12 04:23:45 +0100

    I have edited the class GetTreksQueryHandler of the project core of the solution Voyage

--- Voyage/src/core/Voyage.Application/TrekLists/Queries/GetTreks/GetTreksQueryHandler.cs
+++ Voyage/src/core/Voyage.Application/TrekLists/Queries/GetTreks/GetTreksQueryHandler.cs
@@ -1,7 +1,14 @@
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
 namespace Voyage.Application.TrekLists.Queries.GetTreks
 {
-    public class GetTreksQueryHandler
+    public class GetTreksQueryHandler : IRequestHandler<GetTreksQuery, TreksVm>
     {
-
+        public Task<TreksVm> Handle(GetTreksQuery request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
