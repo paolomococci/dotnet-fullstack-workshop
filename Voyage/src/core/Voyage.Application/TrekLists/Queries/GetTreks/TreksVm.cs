d4d4d09baa1b8416b085f79bf094368210163436
paolomococci <paolomococci@gmail.com>
2021-11-12 04:15:06 +0100

    I have edited the class TreksVm of the project core of the solution Voyage

--- Voyage/src/core/Voyage.Application/TrekLists/Queries/GetTreks/TreksVm.cs
+++ Voyage/src/core/Voyage.Application/TrekLists/Queries/GetTreks/TreksVm.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
+using Voyage.Application.Dtos.Trek;
+
 namespace Voyage.Application.TrekLists.Queries.GetTreks
 {
     public class TreksVm
     {
-
+        public IList<TrekListDto> Lists { get; set; }
     }
 }
