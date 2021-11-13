7cf95fd4be4ccdf48730066bbfe78775ceabe4c7
paolomococci <paolomococci@gmail.com>
2021-11-13 23:49:15 +0100

    I have edited the class UpdateTrekListCommandHandler of the project core of the solution Voyage

--- Voyage/src/core/Voyage.Application/TrekLists/Commands/UpdateTrekList/UpdateTrekListCommandHandler.cs
+++ Voyage/src/core/Voyage.Application/TrekLists/Commands/UpdateTrekList/UpdateTrekListCommandHandler.cs
@@ -1,7 +1,14 @@
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
 namespace Voyage.Application.TrekLists.Commands.UpdateTrekList
 {
-    public class UpdateTrekListCommandHandler
+    public class UpdateTrekListCommandHandler : IRequestHandler<UpdateTrekListCommand>
     {
-
+        public Task<Unit> Handle(UpdateTrekListCommand request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
