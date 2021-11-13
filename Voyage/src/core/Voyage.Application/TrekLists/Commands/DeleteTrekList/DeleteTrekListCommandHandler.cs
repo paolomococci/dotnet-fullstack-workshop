2cfc28c3ae0a58b092154aa110ef5607083d193b
paolomococci <paolomococci@gmail.com>
2021-11-13 23:24:17 +0100

    I have edited the class DeleteTrekListCommandHandler of the project core of the solution Voyage

--- Voyage/src/core/Voyage.Application/TrekLists/Commands/DeleteTrekList/DeleteTrekListCommandHandler.cs
+++ Voyage/src/core/Voyage.Application/TrekLists/Commands/DeleteTrekList/DeleteTrekListCommandHandler.cs
@@ -1,12 +1,42 @@
-using System;
-using System.Collections.Generic;
+
+using MediatR;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using Voyage.Application.Common.Exceptions;
+using Voyage.Application.Common.Interfaces;
+using Voyage.Domain.Entities;
 
 namespace Voyage.Application.TrekLists.Commands.DeleteTrekList
 {
-    public class DeleteTrekListCommandHandler
+    public class DeleteTrekListCommandHandler : IRequestHandler<DeleteTrekListCommand>
     {
-        
+        private readonly IApplicationDbContext _iApplicationDbContext;
+
+        public DeleteTrekListCommandHandler(IApplicationDbContext context)
+        {
+            _iApplicationDbContext = context;
+        }
+
+        public async Task<Unit> Handle(
+            DeleteTrekListCommand request, CancellationToken cancellationToken
+        )
+        {
+            var entity = await _iApplicationDbContext.TrekLists.Where(
+                trekList => trekList.Id == request.Id
+            ).SingleOrDefaultAsync(cancellationToken);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(TrekList), request.Id);
+            }
+
+            _iApplicationDbContext.TrekLists.Remove(entity);
+
+            await _iApplicationDbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
     }
 }
