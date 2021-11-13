9e7b31c138104a08bd991d4b16ec3b29c920ee59
paolomococci <paolomococci@gmail.com>
2021-11-13 23:19:46 +0100

    I added the class DeleteTrekListCommandHandler to the project core of the solution Voyage

---
+++ Voyage/src/core/Voyage.Application/TrekLists/Commands/DeleteTrekList/DeleteTrekListCommandHandler.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Voyage.Application.TrekLists.Commands.DeleteTrekList
+{
+    public class DeleteTrekListCommandHandler
+    {
+        
+    }
+}
