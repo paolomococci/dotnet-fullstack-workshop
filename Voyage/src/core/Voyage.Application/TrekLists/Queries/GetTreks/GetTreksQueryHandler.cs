fe9bdca198bfd4d1a09e315d537ee91072820b44
paolomococci <paolomococci@gmail.com>
2021-11-13 21:19:44 +0100

    I went back to editing class IApplicationDbContext of project core of solution Voyage

--- Voyage/src/core/Voyage.Application/TrekLists/Queries/GetTreks/GetTreksQueryHandler.cs
+++ Voyage/src/core/Voyage.Application/TrekLists/Queries/GetTreks/GetTreksQueryHandler.cs
@@ -1,14 +1,39 @@
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using MediatR;
+using Voyage.Application.Common.Interfaces;
+using Voyage.Application.Dtos.Trek;
 
 namespace Voyage.Application.TrekLists.Queries.GetTreks
 {
     public class GetTreksQueryHandler : IRequestHandler<GetTreksQuery, TreksVm>
     {
-        public Task<TreksVm> Handle(GetTreksQuery request, CancellationToken cancellationToken)
+        private readonly IApplicationDbContext _iApplicationDbContext;
+        private readonly IMapper _iMapper;
+
+        public GetTreksQueryHandler(
+            IApplicationDbContext context,
+            IMapper mapper
+        )
         {
-            throw new System.NotImplementedException();
+            _iApplicationDbContext = context;
+            _iMapper = mapper;
+        }
+
+        public async Task<TreksVm> Handle(GetTreksQuery request, CancellationToken cancellationToken)
+        {
+            return new TreksVm
+            {
+                Lists = await _iApplicationDbContext
+                    .TrekLists
+                    .ProjectTo<TrekListDto>(_iMapper.ConfigurationProvider)
+                    .OrderBy(trekListDto => trekListDto.City)
+                    .ToListAsync(cancellationToken)
+            };
         }
     }
 }
