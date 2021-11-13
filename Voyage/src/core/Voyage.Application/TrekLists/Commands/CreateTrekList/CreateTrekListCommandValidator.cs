498efa11563408c7b5d0f83c01cf97a92e628f37
paolomococci <paolomococci@gmail.com>
2021-11-13 22:44:08 +0100

    I have edited the class CreateTrekListCommandValidator of the project core of the solution Voyage

--- Voyage/src/core/Voyage.Application/TrekLists/Commands/CreateTrekList/CreateTrekListCommandValidator.cs
+++ Voyage/src/core/Voyage.Application/TrekLists/Commands/CreateTrekList/CreateTrekListCommandValidator.cs
@@ -1,7 +1,24 @@
+using FluentValidation;
+using Voyage.Application.Common.Interfaces;
+
 namespace Voyage.Application.TrekLists.Commands.CreateTrekList
 {
-    public class CreateTrekListCommandValidator
+    public class CreateTrekListCommandValidator : AbstractValidator<CreateTrekListCommand>
     {
+        private readonly IApplicationDbContext _context;
 
+        public CreateTrekListCommandValidator(IApplicationDbContext context)
+        {
+            _context = context;
+            /*
+            RuleFor(v => v.Country)
+                .NotEmpty().WithMessage("Country field is required")
+                .MaximumLength(128).WithMessage("The Country field accepts a maximum of 128 characters");
+
+            RuleFor(v => v.City)
+                .NotEmpty().WithMessage("City field is required")
+                .MaximumLength(128).WithMessage("The City field accepts a maximum of 128 characters");
+            */
+        }
     }
 }
