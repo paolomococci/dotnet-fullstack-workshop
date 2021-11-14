d05ef23e53e825cba8d50ccf03cc90e69d71fc62
paolomococci <paolomococci@gmail.com>
2021-11-14 07:33:11 +0100

    I have edited the class CreateTrekPackageCommandHandler of the project core of the solution Voyage

--- Voyage/src/core/Voyage.Application/TrekPackages/Commands/CreateTrekPackage/CreateTrekPackageCommandHandler.cs
+++ Voyage/src/core/Voyage.Application/TrekPackages/Commands/CreateTrekPackage/CreateTrekPackageCommandHandler.cs
@@ -1,7 +1,45 @@
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Voyage.Application.Common.Interfaces;
+using Voyage.Domain.Entities;
+
 namespace Voyage.Application.TrekPackages.Commands.CreateTrekPackage
 {
-    public class CreateTrekPackageCommandHandler
+    public class CreateTrekPackageCommandHandler : IRequestHandler<CreateTrekPackageCommand, int>
     {
 
+        private readonly IApplicationDbContext _iApplicationDbContext;
+
+        public CreateTrekPackageCommandHandler(
+            IApplicationDbContext context
+        )
+        {
+            _iApplicationDbContext = context;
+        }
+
+        public async Task<int> Handle(
+            CreateTrekPackageCommand request,
+            CancellationToken cancellationToken
+        )
+        {
+            var trekPackageEntity = new TrekPackage
+            {
+                ListId = request.ListId,
+                Name = request.Name,
+                Hope = request.Hope,
+                MapLocation = request.MapLocation,
+                Price = request.Price,
+                Duration = request.Duration,
+                Confirmation = request.Confirmation,
+                Currency = request.Currency
+            };
+
+            _iApplicationDbContext.TrekPackages.Add(trekPackageEntity);
+
+            await _iApplicationDbContext.SaveChangesAsync(cancellationToken);
+
+            return trekPackageEntity.Id;
+        }
     }
 }
