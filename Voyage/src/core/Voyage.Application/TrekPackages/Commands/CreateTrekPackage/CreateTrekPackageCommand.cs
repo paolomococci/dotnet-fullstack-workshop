cf91ee665e8bdba27b51f6bdc5023b01d33de27c
paolomococci <paolomococci@gmail.com>
2021-11-14 06:53:16 +0100

    I have edited the class CreateTrekPackageCommand of the project core of the solution Voyage

--- Voyage/src/core/Voyage.Application/TrekPackages/Commands/CreateTrekPackage/CreateTrekPackageCommand.cs
+++ Voyage/src/core/Voyage.Application/TrekPackages/Commands/CreateTrekPackage/CreateTrekPackageCommand.cs
@@ -1,7 +1,18 @@
+using MediatR;
+using Voyage.Domain.Enums;
+
 namespace Voyage.Application.TrekPackages.Commands.CreateTrekPackage
 {
-    public class CreateTrekPackageCommand
+    public class CreateTrekPackageCommand : IRequest<int>
     {
-
+        public int Id { get; set; }
+        public int ListId { get; set; }
+        public string Name { get; set; }
+        public string Hope { get; set; }
+        public string MapLocation { get; set; }
+        public float Price { get; set; }
+        public int Duration { get; set; }
+        public bool Confirmation { get; set; }
+        public Currency Currency { get; set; }
     }
 }
