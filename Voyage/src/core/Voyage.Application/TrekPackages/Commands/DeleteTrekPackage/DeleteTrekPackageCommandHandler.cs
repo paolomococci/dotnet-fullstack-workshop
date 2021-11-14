fef231e36cab7fa3e96af1c49c29eeec33a1aa8f
paolomococci <paolomococci@gmail.com>
2021-11-14 09:08:41 +0100

    I have edited the class DeleteTrekPackageCommandHandler of the project core of the solution Voyage

--- Voyage/src/core/Voyage.Application/TrekPackages/Commands/DeleteTrekPackage/DeleteTrekPackageCommandHandler.cs
+++ Voyage/src/core/Voyage.Application/TrekPackages/Commands/DeleteTrekPackage/DeleteTrekPackageCommandHandler.cs
@@ -1,7 +1,40 @@
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Voyage.Application.Common.Exceptions;
+using Voyage.Application.Common.Interfaces;
+using Voyage.Domain.Entities;
+
 namespace Voyage.Application.TrekPackages.Commands.DeleteTrekPackage
 {
-    public class DeleteTrekPackageCommandHandler
+    public class DeleteTrekPackageCommandHandler : IRequestHandler<DeleteTrekPackageCommand>
     {
+        private readonly IApplicationDbContext _iApplicationDbContext;
 
+        public DeleteTrekPackageCommandHandler(
+            IApplicationDbContext context
+        )
+        {
+            _iApplicationDbContext = context;
+        }
+
+        public async Task<Unit> Handle(
+            DeleteTrekPackageCommand request,
+            CancellationToken cancellationToken
+        )
+        {
+            var trekPackageEntity = await _iApplicationDbContext.TrekPackages.FindAsync(request.Id);
+
+            if (trekPackageEntity == null)
+            {
+                throw new NotFoundException(nameof(TrekPackage), request.Id);
+            }
+
+            _iApplicationDbContext.TrekPackages.Remove(trekPackageEntity);
+
+            await _iApplicationDbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
     }
 }
