3180f0c489c9405ce614d007287d22f46914bbd8
paolomococci <paolomococci@gmail.com>
2021-11-14 17:00:21 +0100

    I have edited the class EmailService of the project infrastructure of the solution Voyage

--- Voyage/src/infrastructure/Voyage.Shared/Services/EmailService.cs
+++ Voyage/src/infrastructure/Voyage.Shared/Services/EmailService.cs
@@ -1,7 +1,14 @@
+using System.Threading.Tasks;
+using Voyage.Application.Common.Interfaces;
+using Voyage.Application.Dtos.Email;
+
 namespace Voyage.Shared.Services
 {
-    public class EmailService
+    public class EmailService : IEmailService
     {
-
+        public Task SendAsync(EmailDto emailRequest)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
