2989cb5698f5d9071f2d8db61a2eacbcd2728709
paolomococci <paolomococci@gmail.com>
2021-11-14 16:55:41 +0100

    I have edited the class CsvFileBuilder of the project infrastructure of the solution Voyage

--- Voyage/src/infrastructure/Voyage.Shared/Files/CsvFileBuilder.cs
+++ Voyage/src/infrastructure/Voyage.Shared/Files/CsvFileBuilder.cs
@@ -1,7 +1,14 @@
+using System.Collections.Generic;
+using Voyage.Application.Common.Interfaces;
+using Voyage.Application.TrekLists.Queries.ExportTreks;
+
 namespace Voyage.Shared.Files
 {
-    public class CsvFileBuilder
+    public class CsvFileBuilder : ICsvFileBuilder
     {
-
+        public byte[] BuildTrekPackagesFile(IEnumerable<TrekPackageRecord> records)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
