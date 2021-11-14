076a13c72fbd1812ccb982cb89ab69a6b28b22dd
paolomococci <paolomococci@gmail.com>
2021-11-14 17:18:17 +0100

    I went back to editing class EmailService of project infrastructure of solution Voyage

--- Voyage/src/infrastructure/Voyage.Shared/Services/EmailService.cs
+++ Voyage/src/infrastructure/Voyage.Shared/Services/EmailService.cs
@@ -1,14 +1,38 @@
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Voyage.Application.Common.Exceptions;
 using Voyage.Application.Common.Interfaces;
 using Voyage.Application.Dtos.Email;
+using Voyage.Domain.Settings;
 
 namespace Voyage.Shared.Services
 {
     public class EmailService : IEmailService
     {
-        public Task SendAsync(EmailDto emailRequest)
+        private MailSettings MailSettings { get; }
+        private ILogger<EmailService> Logger { get; }
+
+        public EmailService(
+            IOptions<MailSettings> mailSettings,
+            ILogger<EmailService> logger
+        )
         {
-            throw new System.NotImplementedException();
+            MailSettings = mailSettings.Value;
+            Logger = logger;
+        }
+
+        public async Task SendAsync(EmailDto emailRequest)
+        {
+            try
+            {
+                // TODO
+            }
+            catch (System.Exception systemException)
+            {
+                Logger.LogError(systemException.Message, systemException);
+                throw new ApiException(systemException.Message);
+            }
         }
     }
 }
