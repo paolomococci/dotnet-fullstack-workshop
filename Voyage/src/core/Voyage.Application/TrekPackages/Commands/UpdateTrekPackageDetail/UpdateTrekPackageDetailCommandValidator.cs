c7e04b470f3ae15b10465c5fb776a85526164558
paolomococci <paolomococci@gmail.com>
2021-11-14 11:19:43 +0100

    I went back to editing class UpdateTrekPackageDetailCommandValidator of project core of solution Voyage

--- Voyage/src/core/Voyage.Application/TrekPackages/Commands/UpdateTrekPackageDetail/UpdateTrekPackageDetailCommandValidator.cs
+++ Voyage/src/core/Voyage.Application/TrekPackages/Commands/UpdateTrekPackageDetail/UpdateTrekPackageDetailCommandValidator.cs
@@ -12,6 +12,30 @@ namespace Voyage.Application.TrekPackages.Commands.UpdateTrekPackageDetail
         )
         {
             _iApplicationDbContext = context;
+
+            RuleFor(updateTrekPackageDetailCommand => updateTrekPackageDetailCommand.ListId)
+                .NotEmpty().WithMessage("ListId field is required");
+
+            RuleFor(updateTrekPackageDetailCommand => updateTrekPackageDetailCommand.Name)
+                .NotEmpty().WithMessage("Name field is required");
+
+            RuleFor(updateTrekPackageDetailCommand => updateTrekPackageDetailCommand.Hope)
+                .NotEmpty().WithMessage("Hope field is required");
+
+            RuleFor(updateTrekPackageDetailCommand => updateTrekPackageDetailCommand.MapLocation)
+                .NotEmpty().WithMessage("MapLocation field is required");
+
+            RuleFor(updateTrekPackageDetailCommand => updateTrekPackageDetailCommand.Price)
+                .NotEmpty().WithMessage("Price field is required");
+
+            RuleFor(updateTrekPackageDetailCommand => updateTrekPackageDetailCommand.Duration)
+                .NotEmpty().WithMessage("Duration field is required");
+
+            RuleFor(updateTrekPackageDetailCommand => updateTrekPackageDetailCommand.Confirmation)
+                .NotEmpty().WithMessage("Confirmation field is required");
+
+            RuleFor(updateTrekPackageDetailCommand => updateTrekPackageDetailCommand.Currency)
+                .NotEmpty().WithMessage("Currency field is required");
         }
     }
 }
