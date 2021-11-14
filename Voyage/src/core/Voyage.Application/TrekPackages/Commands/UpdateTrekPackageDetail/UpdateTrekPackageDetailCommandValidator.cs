988f941fcc907b05779632adb90dae0738abc320
paolomococci <paolomococci@gmail.com>
2021-11-14 11:11:47 +0100

    I have edited the class UpdateTrekPackageDetailCommandValidator of the project core of the solution Voyage

--- Voyage/src/core/Voyage.Application/TrekPackages/Commands/UpdateTrekPackageDetail/UpdateTrekPackageDetailCommandValidator.cs
+++ Voyage/src/core/Voyage.Application/TrekPackages/Commands/UpdateTrekPackageDetail/UpdateTrekPackageDetailCommandValidator.cs
@@ -1,7 +1,17 @@
+using FluentValidation;
+using Voyage.Application.Common.Interfaces;
+
 namespace Voyage.Application.TrekPackages.Commands.UpdateTrekPackageDetail
 {
-    public class UpdateTrekPackageDetailCommandValidator
+    public class UpdateTrekPackageDetailCommandValidator : AbstractValidator<UpdateTrekPackageDetailCommand>
     {
+        private readonly IApplicationDbContext _iApplicationDbContext;
 
+        public UpdateTrekPackageDetailCommandValidator(
+            IApplicationDbContext context
+        )
+        {
+            _iApplicationDbContext = context;
+        }
     }
 }
