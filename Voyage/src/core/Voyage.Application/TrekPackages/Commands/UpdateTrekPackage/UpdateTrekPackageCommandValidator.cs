7f82a774e45730d8ec190348f90d5b56855e49b6
paolomococci <paolomococci@gmail.com>
2021-11-14 10:08:09 +0100

    I went back to editing class UpdateTrekPackageCommandValidator of project core of solution Voyage

--- Voyage/src/core/Voyage.Application/TrekPackages/Commands/UpdateTrekPackage/UpdateTrekPackageCommandValidator.cs
+++ Voyage/src/core/Voyage.Application/TrekPackages/Commands/UpdateTrekPackage/UpdateTrekPackageCommandValidator.cs
@@ -1,9 +1,34 @@
+using System.Threading;
+using System.Threading.Tasks;
 using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Voyage.Application.Common.Interfaces;
 
 namespace Voyage.Application.TrekPackages.Commands.UpdateTrekPackage
 {
     public class UpdateTrekPackageCommandValidator : AbstractValidator<UpdateTrekPackageCommand>
     {
+        private readonly IApplicationDbContext _iApplicationDbContext;
 
+        public UpdateTrekPackageCommandValidator(
+            IApplicationDbContext context
+        )
+        {
+            _iApplicationDbContext = context;
+
+            RuleFor(updateTrekPackageCommand => updateTrekPackageCommand.Name)
+                .NotEmpty().WithMessage("Name field is required.")
+                .MaximumLength(128).WithMessage("Name field must not exceed 128 characters.")
+                .MustAsync(NameFieldMustBeUnique).WithMessage("The specified name field already used.");
+        }
+
+        public async Task<bool> NameFieldMustBeUnique(
+            string name, CancellationToken cancellationToken
+        )
+        {
+            return await _iApplicationDbContext.TrekPackages.AllAsync(
+                trekPackage => trekPackage.Name != name
+            );
+        }
     }
 }
