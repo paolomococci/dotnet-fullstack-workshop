62611af3c9a47f5a9859c66b3fb56a29e9778f50
paolomococci <paolomococci@gmail.com>
2021-11-14 05:55:42 +0100

    I went back to editing class UpdateTrekListCommandHandler of project core of solution Voyage

--- Voyage/src/core/Voyage.Application/TrekLists/Commands/UpdateTrekList/UpdateTrekListCommandHandler.cs
+++ Voyage/src/core/Voyage.Application/TrekLists/Commands/UpdateTrekList/UpdateTrekListCommandHandler.cs
@@ -1,14 +1,41 @@
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using Voyage.Application.Common.Exceptions;
+using Voyage.Application.Common.Interfaces;
+using Voyage.Domain.Entities;
 
 namespace Voyage.Application.TrekLists.Commands.UpdateTrekList
 {
     public class UpdateTrekListCommandHandler : IRequestHandler<UpdateTrekListCommand>
     {
-        public Task<Unit> Handle(UpdateTrekListCommand request, CancellationToken cancellationToken)
+
+        private readonly IApplicationDbContext _iApplicationDbContext;
+
+        public UpdateTrekListCommandHandler(
+            IApplicationDbContext context
+        )
         {
-            throw new System.NotImplementedException();
+            _iApplicationDbContext = context;
+        }
+
+        public async Task<Unit> Handle(
+            UpdateTrekListCommand request,
+            CancellationToken cancellationToken
+        )
+        {
+            var trekListEntity = await _iApplicationDbContext.TrekLists.FindAsync(request.Id);
+
+            if (trekListEntity == null)
+            {
+                throw new NotFoundException(nameof(TrekList), request.Id);
+            }
+
+            trekListEntity.City = request.City;
+
+            await _iApplicationDbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
         }
     }
 }
