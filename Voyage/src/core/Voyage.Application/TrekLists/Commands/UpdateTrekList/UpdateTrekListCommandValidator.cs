dd6b695de81c34c7d3fa735150f36cd83365ca44
paolomococci <paolomococci@gmail.com>
2021-11-14 05:59:39 +0100

    I went back to editing class UpdateTrekListCommandValidator of project core of solution Voyage

--- Voyage/src/core/Voyage.Application/TrekLists/Commands/UpdateTrekList/UpdateTrekListCommandValidator.cs
+++ Voyage/src/core/Voyage.Application/TrekLists/Commands/UpdateTrekList/UpdateTrekListCommandValidator.cs
@@ -1,10 +1,23 @@
 using FluentValidation;
-using MediatR;
+using Voyage.Application.Common.Interfaces;
 
 namespace Voyage.Application.TrekLists.Commands.UpdateTrekList
 {
     public class UpdateTrekListCommandValidator : AbstractValidator<UpdateTrekListCommand>
     {
+        private readonly IApplicationDbContext _iApplicationDbContext;
 
+        public UpdateTrekListCommandValidator(IApplicationDbContext context)
+        {
+            _iApplicationDbContext = context;
+
+            RuleFor(updateTrekListCommand => updateTrekListCommand.Country)
+                .NotEmpty().WithMessage("Country field is required")
+                .MaximumLength(128).WithMessage("The Country field accepts a maximum of 128 characters");
+
+            RuleFor(updateTrekListCommand => updateTrekListCommand.City)
+                .NotEmpty().WithMessage("City field is required")
+                .MaximumLength(128).WithMessage("The City field accepts a maximum of 128 characters");
+        }
     }
 }
