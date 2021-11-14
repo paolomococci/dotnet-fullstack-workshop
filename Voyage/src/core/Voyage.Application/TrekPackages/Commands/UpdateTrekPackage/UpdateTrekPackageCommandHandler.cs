5f6dfb20d7d63e211f3abb5e5a32f19c447e5f6b
paolomococci <paolomococci@gmail.com>
2021-11-14 10:00:08 +0100

    I went back to editing class UpdateTrekPackageCommandHandler of project core of solution Voyage

--- Voyage/src/core/Voyage.Application/TrekPackages/Commands/UpdateTrekPackage/UpdateTrekPackageCommandHandler.cs
+++ Voyage/src/core/Voyage.Application/TrekPackages/Commands/UpdateTrekPackage/UpdateTrekPackageCommandHandler.cs
@@ -1,14 +1,40 @@
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using Voyage.Application.Common.Exceptions;
+using Voyage.Application.Common.Interfaces;
+using Voyage.Domain.Entities;
 
 namespace Voyage.Application.TrekPackages.Commands.UpdateTrekPackage
 {
     public class UpdateTrekPackageCommandHandler : IRequestHandler<UpdateTrekPackageCommand>
     {
-        public Task<Unit> Handle(UpdateTrekPackageCommand request, CancellationToken cancellationToken)
+        private readonly IApplicationDbContext _iApplicationDbContext;
+
+        public UpdateTrekPackageCommandHandler(
+            IApplicationDbContext context
+        )
         {
-            throw new System.NotImplementedException();
+            _iApplicationDbContext = context;
+        }
+
+        public async Task<Unit> Handle(
+            UpdateTrekPackageCommand request,
+            CancellationToken cancellationToken
+        )
+        {
+            var trekPackageEntity = await _iApplicationDbContext.TrekPackages.FindAsync(request.Id);
+
+            if (trekPackageEntity == null)
+            {
+                throw new NotFoundException(nameof(TrekPackage), request.Id);
+            }
+
+            trekPackageEntity.Name = request.Name;
+
+            await _iApplicationDbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
         }
     }
 }
