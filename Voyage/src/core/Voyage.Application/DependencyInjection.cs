ab768eafd733a34f9504c03e1751a8b6ddc08484
paolomococci <paolomococci@gmail.com>
2021-11-14 16:35:21 +0100

    I have edited the class IServiceCollection of the project core of the solution Voyage

--- Voyage/src/core/Voyage.Application/DependencyInjection.cs
+++ Voyage/src/core/Voyage.Application/DependencyInjection.cs
@@ -1,8 +1,25 @@
+using System.Reflection;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Voyage.Application.Common.Behaviors;
+
 namespace Voyage.Application
 {
     public static class DependencyInjection
     {
+        public static IServiceCollection AddApplication(
+            this IServiceCollection services
+        )
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehavior<,>));
 
+            return services;
+        }
     }
 }
-
