6dc8f0749d947bc0188a82889bb265d70451cb50
paolomococci <paolomococci@gmail.com>
2021-11-15 15:14:55 +0100

    I went back to editing class ApiExceptionFilter of project presentation of solution Voyage

--- Voyage/src/presentation/Voyage.WebApi/Filters/ApiExceptionFilter.cs
+++ Voyage/src/presentation/Voyage.WebApi/Filters/ApiExceptionFilter.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Voyage.Application.Common.Exceptions;
 
@@ -22,35 +24,78 @@ namespace Voyage.WebApi.Filters
             ExceptionContext exceptionContext
         )
         {
-            // TODO
+            HandleException(exceptionContext);
+
+            base.OnException(exceptionContext);
         }
 
         private void HandleException(
             ExceptionContext exceptionContext
         )
         {
-            // TODO
+            Type type = exceptionContext.Exception.GetType();
+
+            if (_iDictionaryExceptionHandler.ContainsKey(type))
+            {
+                _iDictionaryExceptionHandler[type]
+                    .Invoke(exceptionContext);
+
+                return;
+            }
+
+            HandleUnknownException(exceptionContext);
         }
 
         private void HandleNotFoundException(
             ExceptionContext exceptionContext
         )
         {
-            // TODO
+            var notFoundException = exceptionContext.Exception as NotFoundException;
+
+            var problemDetails = new ProblemDetails()
+            {
+                // TODO
+                Detail = notFoundException.Message
+            };
+
+            exceptionContext.Result = new NotFoundObjectResult(problemDetails);
+
+            exceptionContext.ExceptionHandled = true;
         }
 
         private void HandleUnknownException(
             ExceptionContext exceptionContext
         )
         {
-            // TODO
+            var problemDetails = new ProblemDetails
+            {
+                Status = StatusCodes.Status500InternalServerError
+                // TODO
+            };
+
+            exceptionContext.Result = new ObjectResult(problemDetails)
+            {
+                StatusCode = StatusCodes.Status500InternalServerError
+            };
+
+            exceptionContext.ExceptionHandled = true;
         }
 
         private void HandleValidationException(
             ExceptionContext exceptionContext
         )
         {
-            // TODO
+            var exception = exceptionContext.Exception as ValidationException;
+
+            var problemDetails = new ProblemDetails()
+            {
+                // TODO
+                Detail = exception.Message
+            };
+
+            exceptionContext.Result = new NotFoundObjectResult(problemDetails);
+
+            exceptionContext.ExceptionHandled = true;
         }
     }
 }
