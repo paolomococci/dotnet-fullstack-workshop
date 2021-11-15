64b0a18c5395f9012dc2f095b2a58dcc583a04cc
paolomococci <paolomococci@gmail.com>
2021-11-15 14:53:28 +0100

    I have edited the class ValidationException of the project core of the solution Voyage

--- Voyage/src/core/Voyage.Application/Common/Exceptions/ValidationException.cs
+++ Voyage/src/core/Voyage.Application/Common/Exceptions/ValidationException.cs
@@ -1,7 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentValidation.Results;
+
 namespace Voyage.Application.Common.Exceptions
 {
-    public class ValidationException
+    public class ValidationException : Exception
     {
+        public IDictionary<string, string[]> validationExceptionErrors { get; }
 
+        public ValidationException()
+            : base("At least one validation error has occurred!")
+        {
+            validationExceptionErrors = new Dictionary<string, string[]>();
+        }
+
+        public ValidationException(
+            IEnumerable<ValidationFailure> failures
+        ) : this()
+        {
+            var failureGroups = failures.GroupBy(
+                validationFailure => validationFailure.PropertyName,
+                validationFailure => validationFailure.ErrorMessage
+            );
+
+            foreach (var failureGroup in failureGroups)
+            {
+                var propertyName = failureGroup.Key;
+                var propertyFailures = failureGroup.ToArray();
+                validationExceptionErrors.Add(propertyName, propertyFailures);
+            }
+        }
     }
 }
