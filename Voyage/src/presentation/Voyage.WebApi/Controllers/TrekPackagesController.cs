eddd218e10866b1e1a0ba1ba8020571629de63bc
paolomococci <paolomococci@gmail.com>
2021-11-15 09:22:01 +0100

    I went back to editing class TrekPackagesController of project presentation of solution Voyage

--- Voyage/src/presentation/Voyage.WebApi/Controllers/TrekPackagesController.cs
+++ Voyage/src/presentation/Voyage.WebApi/Controllers/TrekPackagesController.cs
@@ -6,6 +6,30 @@ namespace Voyage.WebApi.Controllers
     [Route("api/[controller]")]
     public class TrekPackagesController : ApiController
     {
-
+        [HttpPost]
+        public void Create()
+        {
+            // TODO
+        }
+        [HttpGet("{id}")]
+        public void Read()
+        {
+            // TODO
+        }
+        [HttpGet]
+        public void ReadAll()
+        {
+            // TODO
+        }
+        [HttpPut("{id}")]
+        public void Update()
+        {
+            // TODO
+        }
+        [HttpDelete("{id}")]
+        public void Delete()
+        {
+            // TODO
+        }
     }
 }
