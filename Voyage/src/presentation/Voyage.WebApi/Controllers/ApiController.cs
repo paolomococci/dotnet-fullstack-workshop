7824d1fd79dbc8e598e1f46ba20b86e1cf2c2875
paolomococci <paolomococci@gmail.com>
2021-11-15 06:02:17 +0100

    I went back to editing class ApiController of project presentation of solution Voyage

--- Voyage/src/presentation/Voyage.WebApi/Controllers/ApiController.cs
+++ Voyage/src/presentation/Voyage.WebApi/Controllers/ApiController.cs
@@ -1,7 +1,16 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Voyage.WebApi.Controllers
 {
-    public class ApiController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ApiController : ControllerBase
     {
+        private IMediator _iMediator;
 
+        protected IMediator Mediator => _iMediator ??= HttpContext
+            .RequestServices.GetService<IMediator>();
     }
 }
