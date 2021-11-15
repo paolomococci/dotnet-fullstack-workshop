b81ed814bdd2427d61009a7f7a471ea3c81b991a
paolomococci <paolomococci@gmail.com>
2021-11-15 05:30:53 +0100

    I have edited the class Voyage.Shared.DependencyInjection of the project infrastructure of the solution Voyage

--- Voyage/src/infrastructure/Voyage.Shared/DependencyInjection.cs
+++ Voyage/src/infrastructure/Voyage.Shared/DependencyInjection.cs
@@ -1,8 +1,18 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Voyage.Shared
 {
     public static class DependencyInjection
     {
+        public static IServiceCollection AddInfrastructureShared(
+            this IServiceCollection services,
+            IConfiguration config
+        )
+        {
+            // TODO
 
+            return services;
+        }
     }
 }
-
