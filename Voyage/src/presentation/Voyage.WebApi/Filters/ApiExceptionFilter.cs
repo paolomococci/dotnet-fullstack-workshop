dcdfc3413d7c5c0f739f17263a7c5cb93b5ae18d
paolomococci <paolomococci@gmail.com>
2021-11-15 09:06:03 +0100

    I went back to editing class ApiExceptionFilter of project presentation of solution Voyage

--- Voyage/src/presentation/Voyage.WebApi/Filters/ApiExceptionFilter.cs
+++ Voyage/src/presentation/Voyage.WebApi/Filters/ApiExceptionFilter.cs
@@ -1,7 +1,51 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Filters;
+
 namespace Voyage.WebApi.Filters
 {
-    public class ApiExceptionFilter
+    public class ApiExceptionFilter : ExceptionFilterAttribute
     {
+        private readonly IDictionary<Type, Action<ExceptionContext>> _iDictionaryExceptionHandler;
 
+        public ApiExceptionFilter()
+        {
+            // TODO
+        }
+
+        public override void OnException(
+            ExceptionContext exceptionContext
+        )
+        {
+            // TODO
+        }
+
+        private void HandleException(
+            ExceptionContext exceptionContext
+        )
+        {
+            // TODO
+        }
+
+        private void HandleNotFoundException(
+            ExceptionContext exceptionContext
+        )
+        {
+            // TODO
+        }
+
+        private void HandleUnknownException(
+            ExceptionContext exceptionContext
+        )
+        {
+            // TODO
+        }
+
+        private void HandleValidationException(
+            ExceptionContext exceptionContext
+        )
+        {
+            // TODO
+        }
     }
 }
