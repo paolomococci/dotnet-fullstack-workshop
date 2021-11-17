0f68f1cf89c0213c2087d857765f135f466a62ec
paolomococci <paolomococci@gmail.com>
2021-11-17 08:04:51 +0100

    I have edited the class DependencyInjection of the project infrastructure of the solution Voyage

--- Voyage/src/infrastructure/Voyage.Data/DependencyInjection.cs
+++ Voyage/src/infrastructure/Voyage.Data/DependencyInjection.cs
@@ -1,8 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Voyage.Application.Common.Interfaces;
+using Voyage.Data.Contexts;
+
 namespace Voyage.Data
 {
     public static class DependencyInjection
     {
+        public static IServiceCollection AddInfrastructureData(
+            this IServiceCollection services
+        )
+        {
+            services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlite("")
+            );
 
+            services.AddScoped<IApplicationDbContext>(
+                provider => (IApplicationDbContext)provider.GetService<ApplicationDbContext>()
+            );
+
+            return services;
+        }
     }
 }
-
