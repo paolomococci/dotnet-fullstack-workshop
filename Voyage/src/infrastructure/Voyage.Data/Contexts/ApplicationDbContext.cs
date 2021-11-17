e52384d3fe253d595438cc8bd23ce40608d2a4b4
paolomococci <paolomococci@gmail.com>
2021-11-17 08:01:43 +0100

    I went back to editing class ApplicationDbContext

--- Voyage/src/infrastructure/Voyage.Data/Contexts/ApplicationDbContext.cs
+++ Voyage/src/infrastructure/Voyage.Data/Contexts/ApplicationDbContext.cs
@@ -1,15 +1,20 @@
+using System.Data;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
-using Voyage.Application.Common.Interfaces;
+
 using Voyage.Domain.Entities;
+using Voyage.Application.Common.Interfaces;
 
 namespace Voyage.Data.Contexts
 {
     public class ApplicationDbContext : DbContext, IApplicationDbContext
     {
 
-        private readonly IDateTime _dateTime;
-        private IDbContextTransaction _currentTransaction;
+        private readonly IDateTime _iDateTime;
+        private IDbContextTransaction _iDbContextTransaction;
 
         public ApplicationDbContext(
             DbContextOptions<ApplicationDbContext> options
@@ -25,6 +30,77 @@ namespace Voyage.Data.Contexts
         {
             get;
             set;
+        }
+
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options,
+            IDateTime dateTime
+        ) : base(options)
+        {
+            _iDateTime = dateTime;
+        }
+
+        public override Task<int> SaveChangesAsync(
+            CancellationToken cancellationToken = new CancellationToken()
+        )
+        {
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task BeginTransactionAsync()
+        {
+            if (_iDbContextTransaction != null)
+            {
+                return;
+            }
+
+            _iDbContextTransaction = await base.Database
+                .BeginTransactionAsync(IsolationLevel.ReadCommitted)
+                .ConfigureAwait(false);
+        }
+
+        public async Task CommitTransactionAsync()
+        {
+            try
+            {
+                await SaveChangesAsync().ConfigureAwait(false);
+                _iDbContextTransaction?.Commit();
+            }
+            catch
+            {
+                RollbackTransaction();
+                throw;
+            }
+            finally
+            {
+                if (_iDbContextTransaction != null)
+                {
+                    _iDbContextTransaction.Dispose();
+                    _iDbContextTransaction = null;
+                }
+            }
+        }
+
+        private void RollbackTransaction()
+        {
+            try
+            {
+                _iDbContextTransaction?.Rollback();
+            }
+            finally
+            {
+                if (_iDbContextTransaction != null)
+                {
+                    _iDbContextTransaction.Dispose();
+                    _iDbContextTransaction = null;
+                }
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(builder);
         }
     }
 }
