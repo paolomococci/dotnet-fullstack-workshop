a83169a12fa990211960a431f2ee53b83426079f
paolomococci <paolomococci@gmail.com>
2021-11-17 06:37:26 +0100

    I have edited the class VoyageDbContext of the project infrastructure of the solution Voyage

--- Voyage/src/infrastructure/Voyage.Data/Contexts/VoyageDbContext.cs
+++ Voyage/src/infrastructure/Voyage.Data/Contexts/VoyageDbContext.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Voyage.Domain.Entities;
+
 namespace Voyage.Data.Contexts
 {
-    public class VoyageDbContext
+    public class VoyageDbContext : DbContext
     {
-
+        public VoyageDbContext(
+            DbContextOptions<VoyageDbContext> dbContextOptions
+        ) : base(dbContextOptions)
+        { }
+        public DbSet<TrekList> TrekLists { get; set; }
+        public DbSet<TrekPackage> TrekPackages { get; set; }
     }
 }
