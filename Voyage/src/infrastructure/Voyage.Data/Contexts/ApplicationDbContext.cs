17055a6e0f55859e05816cdcc028f2cc9232e803
paolomococci <paolomococci@gmail.com>
2021-11-17 06:41:12 +0100

    I have edited the class ApplicationDbContext of the project infrastructure of the solution Voyage

--- Voyage/src/infrastructure/Voyage.Data/Contexts/ApplicationDbContext.cs
+++ Voyage/src/infrastructure/Voyage.Data/Contexts/ApplicationDbContext.cs
@@ -1,8 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Voyage.Application.Common.Interfaces;
+using Voyage.Domain.Entities;
+
 namespace Voyage.Data.Contexts
 {
-	public class ApplicationDbContext
-	{
-		
-	}
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public DbSet<TrekList> TrekLists
+        {
+            get => throw new System.NotImplementedException();
+            set => throw new System.NotImplementedException();
+        }
+        public DbSet<TrekPackage> TrekPackages
+        {
+            get => throw new System.NotImplementedException();
+            set => throw new System.NotImplementedException();
+        }
+    }
 }
-
