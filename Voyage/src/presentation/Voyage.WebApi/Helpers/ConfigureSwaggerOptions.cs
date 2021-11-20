64f306885fe1d99e023a2a1006b27ff9913f6821
paolomococci <paolomococci@gmail.com>
2021-11-20 16:13:25 +0100

    I have edited the class ConfigureSwaggerOptions of the project presentation of the solution Voyage

--- Voyage/src/presentation/Voyage.WebApi/Helpers/ConfigureSwaggerOptions.cs
+++ Voyage/src/presentation/Voyage.WebApi/Helpers/ConfigureSwaggerOptions.cs
@@ -1,7 +1,13 @@
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
 namespace Voyage.WebApi.Helpers
 {
-    public class ConfigureSwaggerOptions
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
     {
-
+        public void Configure(SwaggerGenOptions options)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
