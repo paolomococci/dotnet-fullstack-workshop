b2f92a652f01fb55b47ba7bcd457450ffd9b28f0
paolomococci <paolomococci@gmail.com>
2021-11-20 16:36:14 +0100

    I went back to editing class SwaggerDefaultValues of project presentation of solution Voyage

--- Voyage/src/presentation/Voyage.WebApi/Helpers/SwaggerDefaultValues.cs
+++ Voyage/src/presentation/Voyage.WebApi/Helpers/SwaggerDefaultValues.cs
@@ -1,3 +1,6 @@
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
@@ -10,7 +13,33 @@ namespace Voyage.WebApi.Helpers
             OperationFilterContext context
         )
         {
-            throw new System.NotImplementedException();
+            var contextApiDescription = context.ApiDescription;
+            operation.Deprecated |= contextApiDescription.IsDeprecated();
+
+            if (operation.Parameters == null)
+            {
+                return;
+            }
+
+            foreach (var parameter in operation.Parameters)
+            {
+                var description = contextApiDescription.ParameterDescriptions.First(
+                    apiParameterDescription => apiParameterDescription.Name == parameter.Name
+                );
+
+                parameter.Description ??= description.ModelMetadata.Description;
+
+                if (
+                    parameter.Schema.Default == null && description.DefaultValue != null
+                )
+                {
+                    parameter.Schema.Default = new OpenApiString(
+                        description.DefaultValue.ToString()
+                    );
+                }
+
+                parameter.Required |= description.IsRequired;
+            }
         }
     }
 }
