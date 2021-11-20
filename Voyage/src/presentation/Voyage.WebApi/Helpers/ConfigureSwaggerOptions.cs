bbf85fe8e68c552c5924eb430adc52600079b514
paolomococci <paolomococci@gmail.com>
2021-11-20 16:15:50 +0100

    I have edited the class SwaggerDefaultValues of the project presentation of the solution Voyage

--- Voyage/src/presentation/Voyage.WebApi/Helpers/ConfigureSwaggerOptions.cs
+++ Voyage/src/presentation/Voyage.WebApi/Helpers/ConfigureSwaggerOptions.cs
@@ -5,7 +5,9 @@ namespace Voyage.WebApi.Helpers
 {
     public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
     {
-        public void Configure(SwaggerGenOptions options)
+        public void Configure(
+            SwaggerGenOptions options
+        )
         {
             throw new System.NotImplementedException();
         }
--- Voyage/src/presentation/Voyage.WebApi/Helpers/SwaggerDefaultValues.cs
+++ Voyage/src/presentation/Voyage.WebApi/Helpers/SwaggerDefaultValues.cs
@@ -1,7 +1,16 @@
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
 namespace Voyage.WebApi.Helpers
 {
-    public class SwaggerDefaultValues
+    public class SwaggerDefaultValues : IOperationFilter
     {
-
+        public void Apply(
+            OpenApiOperation operation,
+            OperationFilterContext context
+        )
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
