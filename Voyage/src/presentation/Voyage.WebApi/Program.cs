a14d2b1c61cb8bc0cb18e411b51a2e5b6c421500
paolomococci <paolomococci@gmail.com>
2021-11-21 10:49:47 +0100

    I started reconfiguring solution Voyage for logging

--- Voyage/src/presentation/Voyage.WebApi/Program.cs
+++ Voyage/src/presentation/Voyage.WebApi/Program.cs
@@ -1,11 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Exceptions;
+using Serilog.Formatting.Compact;
 
 namespace Voyage.WebApi
 {
@@ -13,7 +18,28 @@ namespace Voyage.WebApi
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+            var name = Assembly.GetExecutingAssembly().GetName();
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .Enrich.FromLogContext()
+                .Enrich.WithExceptionDetails()
+                .Enrich.WithMachineName()
+                .Enrich.WithProperty("Assembly", $"{name.Name}")
+                .Enrich.WithProperty("Assembly", $"{name.Version}")
+                .WriteTo.SQLite(
+                    Environment.CurrentDirectory + @"/Logs/log.db",
+                    restrictedToMinimumLevel: LogEventLevel.Information,
+                    storeTimestampInUtc: true
+                )
+                .WriteTo.File(
+                    new CompactJsonFormatter(),
+                    Environment.CurrentDirectory + @"/Logs/log.json",
+                    rollingInterval: RollingInterval.Hour,
+                    restrictedToMinimumLevel: LogEventLevel.Debug
+                )
+                .WriteTo.Console().CreateLogger();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
