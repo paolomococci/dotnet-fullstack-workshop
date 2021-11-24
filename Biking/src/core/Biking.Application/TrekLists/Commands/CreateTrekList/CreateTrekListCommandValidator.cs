39562c1e66c562853969b40bee34baf2d5805835
paolomococci <paolomococci@gmail.com>
2021-11-24 16:57:58 +0100

    I have edited the class CreateTrekListCommandValidator of project Biking.Application belonging to solution Biking

--- Biking/src/core/Biking.Application/TrekLists/Commands/CreateTrekList/CreateTrekListCommandValidator.cs
+++ Biking/src/core/Biking.Application/TrekLists/Commands/CreateTrekList/CreateTrekListCommandValidator.cs
@@ -1,7 +1,23 @@
+using Biking.Application.Common.Interfaces;
+using FluentValidation;
+
 namespace Biking.Application.TrekLists.Commands
 {
-    public class CreateTrekListCommandValidator
+    public class CreateTrekListCommandValidator : AbstractValidator<CreateTrekListCommand>
     {
+        private readonly IApplicationDbContext _iApplicationDbContext;
 
+        public CreateTrekListCommandValidator(IApplicationDbContext context)
+        {
+            _iApplicationDbContext = context;
+
+            RuleFor(createTrekListCommand => createTrekListCommand.Country)
+                .NotEmpty().WithMessage("Country field is required")
+                .MaximumLength(128).WithMessage("The Country field accepts a maximum of 128 characters");
+
+            RuleFor(createTrekListCommand => createTrekListCommand.City)
+                .NotEmpty().WithMessage("City field is required")
+                .MaximumLength(128).WithMessage("The City field accepts a maximum of 128 characters");
+        }
     }
 }
