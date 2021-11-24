f2a3c1652d103c4b5453b6adc551d901c7cb7357
paolomococci <paolomococci@gmail.com>
2021-11-24 16:41:34 +0100

    I have edited the class CreateTrekListCommandHandler of project Biking.Application belonging to solution Biking

---
+++ Biking/src/core/Biking.Application/TrekLists/Commands/CreateTrekListCommandHandler.cs
@@ -0,0 +1,34 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Biking.Application.Common.Interfaces;
+using MediatR;
+
+namespace Biking.Application.TrekLists.Commands
+{
+    public class CreateTrekListCommandHandler : IRequestHandler<CreateTrekListCommand, int>
+    {
+        private readonly IApplicationDbContext _iApplicationDbContext;
+
+        public CreateTrekListCommandHandler(IApplicationDbContext context)
+        {
+            _iApplicationDbContext = context;
+        }
+
+        public async Task<int> Handle(
+            CreateTrekListCommand request,
+            CancellationToken cancellationToken
+        )
+        {
+            var trekListEntity = new TrekList
+            {
+                City = request.City
+            };
+
+            _iApplicationDbContext.TrekLists.Add(trekListEntity);
+
+            await _iApplicationDbContext.SaveChangesAsync(cancellationToken);
+
+            return trekListEntity.Id;
+        }
+    }
+}
