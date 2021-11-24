c70145e8dd2f3062f3e911933c66204599dadd23
paolomococci <paolomococci@gmail.com>
2021-11-24 16:03:36 +0100

    I have edited the class PropertyValidationException of project Biking.Application belonging to solution Biking

--- Biking/src/core/Biking.Application/Common/Exceptions/PropertyValidationException.cs
+++ Biking/src/core/Biking.Application/Common/Exceptions/PropertyValidationException.cs
@@ -1,8 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentValidation.Results;
+
 namespace Biking.Application.Common.Exceptions
 {
-	public class PropertyValidationException
-	{
-		
-	}
-}
+    public class PropertyValidationException : Exception
+    {
+        public IDictionary<string, string[]> Errors { get; }
 
+        public PropertyValidationException() : base("Validation failure")
+        {
+            Errors = new Dictionary<string, string[]>();
+        }
+
+        public PropertyValidationException(IEnumerable<ValidationFailure> failures) : this()
+        {
+            var failureGroups = failures.GroupBy(
+                validationFailure => validationFailure.PropertyName,
+                validationFailure => validationFailure.ErrorMessage
+            );
+
+            foreach (var failureGroup in failureGroups)
+            {
+                var propertyName = failureGroup.Key;
+                var propertyFailures = failureGroup.ToArray();
+                Errors.Add(propertyName, propertyFailures);
+            }
+        }
+    }
+}
