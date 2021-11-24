e17291138c55524a734ed749da0ca64eb4c7da5f
paolomococci <paolomococci@gmail.com>
2021-11-24 17:04:06 +0100

    I have edited the class DeleteTrekListCommandHandler of project Biking.Application belonging to solution Biking

--- Biking/src/core/Biking.Application/TrekLists/Commands/DeleteTrekList/DeleteTrekListCommandHandler.cs
+++ Biking/src/core/Biking.Application/TrekLists/Commands/DeleteTrekList/DeleteTrekListCommandHandler.cs
@@ -1,7 +1,39 @@
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Biking.Application.Common.Exceptions;
+using Biking.Application.Common.Interfaces;
+using MediatR;
+
 namespace Biking.Application.TrekLists.Commands.DeleteTrekList
 {
-    public class DeleteTrekListCommandHandler
+    public class DeleteTrekListCommandHandler : IRequestHandler<DeleteTrekListCommand>
     {
+        private readonly IApplicationDbContext _iApplicationDbContext;
 
+        public DeleteTrekListCommandHandler(IApplicationDbContext context)
+        {
+            _iApplicationDbContext = context;
+        }
+
+        public async Task<Unit> Handle(
+            DeleteTrekListCommand request, CancellationToken cancellationToken
+        )
+        {
+            var entity = await _iApplicationDbContext.TrekLists.Where(
+                trekList => trekList.Id == request.Id
+            ).SingleOrDefaultAsync(cancellationToken);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(TrekList), request.Id);
+            }
+
+            _iApplicationDbContext.TrekLists.Remove(entity);
+
+            await _iApplicationDbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
     }
 }
