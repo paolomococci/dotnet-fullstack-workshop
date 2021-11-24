eb56ffb96aeb92e2c5e354532ecd518f0c6bd19c
paolomococci <paolomococci@gmail.com>
2021-11-24 16:24:45 +0100

    I have edited the class TrekListDto of project Biking.Application belonging to solution Biking

---
+++ Biking/src/core/Biking.Application/Dtos/Trek/TrekListDto.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using Biking.Application.Common.Mappings;
+
+namespace Biking.Application.Dtos.Trek
+{
+    public class TrekListDto : IMapFrom<TrekList>
+    {
+        public IList<TrekPackageDto> Items { get; set; }
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+
+        public TrekListDto()
+        {
+            Items = new List<TrekPackageDto>();
+        }
+    }
+}
