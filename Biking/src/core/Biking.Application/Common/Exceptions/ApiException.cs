55ac6e5691f8564480247a07c82395d7aa97efe0
paolomococci <paolomococci@gmail.com>
2021-11-24 15:58:55 +0100

    I have edited the class ApiException of project Biking.Application belonging to solution Biking

--- Biking/src/core/Biking.Application/Common/Exceptions/ApiException.cs
+++ Biking/src/core/Biking.Application/Common/Exceptions/ApiException.cs
@@ -1,8 +1,24 @@
+using System;
+using System.Globalization;
+
 namespace Biking.Application.Common.Exceptions
 {
-	public class ApiException
-	{
-		
-	}
-}
+    public class ApiException : Exception
+    {
+        public ApiException() : base() { }
 
+        public ApiException(string message) : base(message) { }
+
+        public ApiException(
+            string message,
+            params object[] args
+        ) : base(
+            String.Format(
+                CultureInfo.CurrentCulture,
+                message,
+                args
+            )
+        )
+        { }
+    }
+}
