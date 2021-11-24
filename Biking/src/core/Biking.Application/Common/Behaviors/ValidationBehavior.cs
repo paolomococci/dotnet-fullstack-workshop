1c07ef6c6a463c236de438f8151df4364afb18b2
paolomococci <paolomococci@gmail.com>
2021-11-24 15:39:09 +0100

    I have edited the class ValidationBehavior of project Biking.Application belonging to solution Biking

--- Biking/src/core/Biking.Application/Common/Behaviors/ValidationBehavior.cs
+++ Biking/src/core/Biking.Application/Common/Behaviors/ValidationBehavior.cs
@@ -1,8 +1,54 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Biking.Application.Common.Exceptions;
+using FluentValidation;
+using MediatR;
+
 namespace Biking.Application.Common.Behaviors
 {
-	public class ValidationBehavior
-	{
-		
-	}
-}
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _iEnumerableIValidatorTRequest;
 
+        public ValidationBehavior(
+            IEnumerable<IValidator<TRequest>> validators
+        )
+        {
+            _iEnumerableIValidatorTRequest = validators;
+        }
+
+        public async Task<TResponse> Handle(
+            TRequest request,
+            CancellationToken cancellationToken,
+            RequestHandlerDelegate<TResponse> next
+        )
+        {
+            if (!_iEnumerableIValidatorTRequest.Any())
+            {
+                return await next();
+            }
+
+            var context = new ValidationContext<TRequest>(request);
+
+            var validationResults = await Task.WhenAll(
+                _iEnumerableIValidatorTRequest.Select(
+                    validator => validator.ValidateAsync(context, cancellationToken)
+                )
+            );
+
+            var failures = validationResults
+                .SelectMany(validationResult => validationResult.Errors)
+                .Where(validationFailure => validationFailure != null).ToList();
+
+            if (failures.Count != 0)
+            {
+                throw new PropertyValidationException(failures);
+            }
+
+            return await next();
+        }
+    }
+}
