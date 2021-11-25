8d9016b3b6e119ec1231e36b8d62d3780fdae4e5
paolomococci <paolomococci@gmail.com>
2021-11-25 17:54:37 +0100

    I have edited the abstract class ApiController of project Biking.WebApi

--- Biking/src/presentation/Biking.WebApi/Controllers/v1/ApiController.cs
+++ Biking/src/presentation/Biking.WebApi/Controllers/v1/ApiController.cs
@@ -1,7 +1,14 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Biking.WebApi.Controllers.v1
 {
-    public abstract class ApiController
+    public abstract class ApiController : ControllerBase
     {
+        private IMediator _iMediator;
 
+        protected IMediator Mediator => _iMediator ??= HttpContext
+            .RequestServices.GetService<IMediator>();
     }
 }
