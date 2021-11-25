91ed1cbb9e28d7df6aa38096a2b1be876497efc6
paolomococci <paolomococci@gmail.com>
2021-11-25 16:00:00 +0100

    I have edited the class ApplicationDbContext of project Biking.Data

--- Biking/src/infrastructure/Biking.Data/Contexts/ApplicationDbContext.cs
+++ Biking/src/infrastructure/Biking.Data/Contexts/ApplicationDbContext.cs
@@ -1,8 +1,105 @@
+using System.Data;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Biking.Application.Common.Interfaces;
+using Biking.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
 namespace Biking.Data.Contexts
 {
-	public class ApplicationDbContext
-	{
-		
-	}
-}
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
 
+        private readonly IDateTime _iDateTime;
+        private IDbContextTransaction _iDbContextTransaction;
+
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options
+        ) : base(options)
+        { }
+
+        public DbSet<TrekList> TrekLists
+        {
+            get;
+            set;
+        }
+        public DbSet<TrekPackage> TrekPackages
+        {
+            get;
+            set;
+        }
+
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options,
+            IDateTime dateTime
+        ) : base(options)
+        {
+            _iDateTime = dateTime;
+        }
+
+        public override Task<int> SaveChangesAsync(
+            CancellationToken cancellationToken = new CancellationToken()
+        )
+        {
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task BeginTransactionAsync()
+        {
+            if (_iDbContextTransaction != null)
+            {
+                return;
+            }
+
+            _iDbContextTransaction = await base.Database
+                .BeginTransactionAsync(IsolationLevel.ReadCommitted)
+                .ConfigureAwait(false);
+        }
+
+        public async Task CommitTransactionAsync()
+        {
+            try
+            {
+                await SaveChangesAsync().ConfigureAwait(false);
+                _iDbContextTransaction?.Commit();
+            }
+            catch
+            {
+                RollbackTransaction();
+                throw;
+            }
+            finally
+            {
+                if (_iDbContextTransaction != null)
+                {
+                    _iDbContextTransaction.Dispose();
+                    _iDbContextTransaction = null;
+                }
+            }
+        }
+
+        private void RollbackTransaction()
+        {
+            try
+            {
+                _iDbContextTransaction?.Rollback();
+            }
+            finally
+            {
+                if (_iDbContextTransaction != null)
+                {
+                    _iDbContextTransaction.Dispose();
+                    _iDbContextTransaction = null;
+                }
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(builder);
+        }
+    }
+}
