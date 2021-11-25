227ad7825c7f2fc8ad35ec853ac8be17b626f08e
paolomococci <paolomococci@gmail.com>
2021-11-25 06:47:18 +0100

    I have edited the class UpdateTrekListCommandHandler of project Biking.Application belonging to solution Biking

--- Biking/src/core/Biking.Application/TrekLists/Commands/UpdateTrekList/UpdateTrekListCommandHandler.cs
+++ Biking/src/core/Biking.Application/TrekLists/Commands/UpdateTrekList/UpdateTrekListCommandHandler.cs
@@ -1,7 +1,40 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Biking.Application.Common.Exceptions;
+using Biking.Application.Common.Interfaces;
+using MediatR;
+
 namespace Biking.Application.TrekLists.Commands.UpdateTrekList
 {
-    public class UpdateTrekListCommandHandler
+    public class UpdateTrekListCommandHandler : IRequestHandler<UpdateTrekListCommand>
     {
 
+        private readonly IApplicationDbContext _iApplicationDbContext;
+
+        public UpdateTrekListCommandHandler(
+            IApplicationDbContext context
+        )
+        {
+            _iApplicationDbContext = context;
+        }
+
+        public async Task<Unit> Handle(
+            UpdateTrekListCommand request,
+            CancellationToken cancellationToken
+        )
+        {
+            var trekListEntity = await _iApplicationDbContext.TrekLists.FindAsync(request.Id);
+
+            if (trekListEntity == null)
+            {
+                throw new NotFoundException(nameof(TrekList), request.Id);
+            }
+
+            trekListEntity.City = request.City;
+
+            await _iApplicationDbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
     }
 }
