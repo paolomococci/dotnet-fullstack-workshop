b3abc0f7228625f70a656b755e3451565b8ec2e6
paolomococci <paolomococci@gmail.com>
2021-11-25 09:54:54 +0100

    I have edited the class DeleteTrekPackageCommandHandler of project Biking.Application belonging to solution Biking

--- Biking/src/core/Biking.Application/TrekPackages/Commands/DeleteTrekPackage/DeleteTrekPackageCommandHandler.cs
+++ Biking/src/core/Biking.Application/TrekPackages/Commands/DeleteTrekPackage/DeleteTrekPackageCommandHandler.cs
@@ -1,7 +1,39 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Biking.Application.Common.Exceptions;
+using Biking.Application.Common.Interfaces;
+using MediatR;
+
 namespace Biking.Application.TrekPackages.Commands.DeleteTrekPackage
 {
-    public class DeleteTrekPackageCommandHandler
+    public class DeleteTrekPackageCommandHandler : IRequestHandler<DeleteTrekPackageCommand>
     {
+        private readonly IApplicationDbContext _iApplicationDbContext;
 
+        public DeleteTrekPackageCommandHandler(
+            IApplicationDbContext context
+        )
+        {
+            _iApplicationDbContext = context;
+        }
+
+        public async Task<Unit> Handle(
+            DeleteTrekPackageCommand request,
+            CancellationToken cancellationToken
+        )
+        {
+            var trekPackageEntity = await _iApplicationDbContext.TrekPackages.FindAsync(request.Id);
+
+            if (trekPackageEntity == null)
+            {
+                throw new NotFoundException(nameof(TrekPackage), request.Id);
+            }
+
+            _iApplicationDbContext.TrekPackages.Remove(trekPackageEntity);
+
+            await _iApplicationDbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
     }
 }
