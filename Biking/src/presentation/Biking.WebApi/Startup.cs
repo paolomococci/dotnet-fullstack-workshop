dc8d1e4eb1bcf5937b02d23c2c4e967093167a84
paolomococci <paolomococci@gmail.com>
2021-11-25 18:12:37 +0100

    I have edited the class Startup of project Biking.WebApi

--- Biking/src/presentation/Biking.WebApi/Startup.cs
+++ Biking/src/presentation/Biking.WebApi/Startup.cs
@@ -1,16 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Biking.Application;
+using Biking.Data;
+using Biking.Shared;
+using Biking.WebApi.Filters;
+using Biking.WebApi.Helpers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace Biking.WebApi
 {
@@ -26,22 +27,63 @@ namespace Biking.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplication();
+            services.AddInfrastructureData();
+            services.AddInfrastructureShared(Configuration);
+            services.AddHttpContextAccessor();
+
+            services.AddControllersWithViews(
+                options => options.Filters.Add(new ApiExceptionFilter())
+            );
+
+            services.Configure<ApiBehaviorOptions>(
+                options => options.SuppressModelStateInvalidFilter = true
+            );
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
+
+            services.AddSwaggerGen(swaggerUIOptions =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Biking.WebApi", Version = "v1" });
+                swaggerUIOptions.OperationFilter<SwaggerDefaultValues>();
+            });
+
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+
+            services.AddApiVersioning(config =>
+            {
+                config.DefaultApiVersion = new ApiVersion(1, 0);
+                config.AssumeDefaultVersionWhenUnspecified = true;
+                config.ReportApiVersions = true;
+            });
+
+            services.AddVersionedApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(
+            IApplicationBuilder app,
+            IWebHostEnvironment env,
+            IApiVersionDescriptionProvider provider
+        )
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Biking.WebApi v1"));
+                app.UseSwaggerUI(swaggerUIOptions =>
+                {
+                    foreach (var description in provider.ApiVersionDescriptions)
+                    {
+                        swaggerUIOptions.SwaggerEndpoint(
+                            $"/swagger/{description.GroupName}/swagger.json",
+                            description.GroupName.ToUpperInvariant()
+                        );
+                    }
+                });
             }
 
             app.UseHttpsRedirection();
