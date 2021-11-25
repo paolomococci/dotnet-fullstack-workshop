20bcba2148d518e5f4a18798828504e34e9e5e47
paolomococci <paolomococci@gmail.com>
2021-11-25 16:37:28 +0100

    I have edited the class DateTimeService of project Biking.Shared

--- Biking/src/infrastructure/Biking.Shared/Services/DateTimeService.cs
+++ Biking/src/infrastructure/Biking.Shared/Services/DateTimeService.cs
@@ -1,7 +1,10 @@
+using System;
+using Biking.Application.Common.Interfaces;
+
 namespace Biking.Shared.Services
 {
-    public class DateTimeService
+    public class DateTimeService : IDateTime
     {
-
+        public DateTime NowUtc => DateTime.UtcNow;
     }
 }
