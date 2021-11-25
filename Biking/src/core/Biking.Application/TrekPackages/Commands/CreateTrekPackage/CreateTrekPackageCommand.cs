d5332f56e83032d5b74c6b25cbac68634039b79a
paolomococci <paolomococci@gmail.com>
2021-11-25 09:31:03 +0100

    I have edited the class CreateTrekPackageCommand of project Biking.Application belonging to solution Biking

--- Biking/src/core/Biking.Application/TrekPackages/Commands/CreateTrekPackage/CreateTrekPackageCommand.cs
+++ Biking/src/core/Biking.Application/TrekPackages/Commands/CreateTrekPackage/CreateTrekPackageCommand.cs
@@ -1,7 +1,16 @@
+using MediatR;
+
 namespace Biking.Application.TrekPackages.Commands.CreateTrekPackage
 {
-    public class CreateTrekPackageCommand
+    public class CreateTrekPackageCommand : IRequest<int>
     {
-
+        public int ListId { get; set; }
+        public string Name { get; set; }
+        public string Hope { get; set; }
+        public string MapLocation { get; set; }
+        public float Price { get; set; }
+        public int Duration { get; set; }
+        public bool Confirmation { get; set; }
+        public Currency Currency { get; set; }
     }
 }
