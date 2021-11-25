9eb48e962da2e107ce56a613282cf1cd1b5a5f96
paolomococci <paolomococci@gmail.com>
2021-11-25 09:37:51 +0100

    I have edited the class CreateTrekPackageCommandValidator of project Biking.Application belonging to solution Biking

--- Biking/src/core/Biking.Application/TrekPackages/Commands/CreateTrekPackage/CreateTrekPackageCommandValidator.cs
+++ Biking/src/core/Biking.Application/TrekPackages/Commands/CreateTrekPackage/CreateTrekPackageCommandValidator.cs
@@ -1,7 +1,35 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Biking.Application.Common.Interfaces;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
 namespace Biking.Application.TrekPackages.Commands.CreateTrekPackage
 {
-    public class CreateTrekPackageCommandValidator
+    public class CreateTrekPackageCommandValidator : AbstractValidator<CreateTrekPackageCommand>
     {
+        private readonly IApplicationDbContext _iApplicationDbContext;
 
+        public CreateTrekPackageCommandValidator(IApplicationDbContext context)
+        {
+            _iApplicationDbContext = context;
+
+            RuleFor(createTrekPackageCommand => createTrekPackageCommand.Name)
+                .NotEmpty().WithMessage("Name field is required.")
+                .MaximumLength(128).WithMessage("Name field must not exceed 128 characters.")
+                .MustAsync(NameFieldMustBeUnique).WithMessage("The specified name field already used.");
+
+            RuleFor(createTrekPackageCommand => createTrekPackageCommand.ListId)
+                .NotEmpty().WithMessage("ListId field is required");
+        }
+
+        public async Task<bool> NameFieldMustBeUnique(
+            string name, CancellationToken cancellationToken
+        )
+        {
+            return await _iApplicationDbContext.TrekPackages.AllAsync(
+                trekPackage => trekPackage.Name != name
+            );
+        }
     }
 }
