651c1b03e09f48f627ec4d0e098b7a9c7341133e
paolomococci <paolomococci@gmail.com>
2021-11-25 05:55:53 +0100

    I have edited the class UpdateTrekListCommand of project Biking.Application belonging to solution Biking

--- Biking/src/core/Biking.Application/TrekLists/Commands/UpdateTrekList/UpdateTrekListCommand.cs
+++ Biking/src/core/Biking.Application/TrekLists/Commands/UpdateTrekList/UpdateTrekListCommand.cs
@@ -1,7 +1,11 @@
+using MediatR;
+
 namespace Biking.Application.TrekLists.Commands.UpdateTrekList
 {
-    public class UpdateTrekListCommand
+    public class UpdateTrekListCommand : IRequest
     {
-
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
     }
 }
