40f5fdd8f7b92e73219bd81922f579405c67be85
paolomococci <paolomococci@gmail.com>
2021-11-25 10:07:09 +0100

    I have edited the class UpdateTrekPackageCommandHandler of project Biking.Application belonging to solution Biking

--- Biking/src/core/Biking.Application/TrekPackages/Commands/UpdateTrekPackage/UpdateTrekPackageCommandHandler.cs
+++ Biking/src/core/Biking.Application/TrekPackages/Commands/UpdateTrekPackage/UpdateTrekPackageCommandHandler.cs
@@ -1,7 +1,39 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Biking.Application.Common.Exceptions;
+using Biking.Application.Common.Interfaces;
+using MediatR;
+
 namespace Biking.Application.TrekPackages.Commands.UpdateTrekPackage
 {
-    public class UpdateTrekPackageCommandHandler
+    public class UpdateTrekPackageCommandHandler : IRequestHandler<UpdateTrekPackageCommand>
     {
+        private readonly IApplicationDbContext _iApplicationDbContext;
 
+        public UpdateTrekPackageCommandHandler(
+            IApplicationDbContext context
+        )
+        {
+            _iApplicationDbContext = context;
+        }
+
+        public async Task<Unit> Handle(
+            UpdateTrekPackageCommand request,
+            CancellationToken cancellationToken
+        )
+        {
+            var trekPackageEntity = await _iApplicationDbContext.TrekPackages.FindAsync(request.Id);
+
+            if (trekPackageEntity == null)
+            {
+                throw new NotFoundException(nameof(TrekPackage), request.Id);
+            }
+
+            trekPackageEntity.Name = request.Name;
+
+            await _iApplicationDbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
     }
 }
