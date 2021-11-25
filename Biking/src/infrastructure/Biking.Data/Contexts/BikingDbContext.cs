659b574def9310de5524d2952d1927c7fe945a84
paolomococci <paolomococci@gmail.com>
2021-11-25 16:03:50 +0100

    I have edited the class BikingDbContext of project Biking.Data

--- Biking/src/infrastructure/Biking.Data/Contexts/BikingDbContext.cs
+++ Biking/src/infrastructure/Biking.Data/Contexts/BikingDbContext.cs
@@ -1,7 +1,15 @@
+using Biking.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace Biking.Data.Contexts
 {
-    public class BikingDbContext
+    public class BikingDbContext : DbContext
     {
-
+        public BikingDbContext(
+            DbContextOptions<BikingDbContext> dbContextOptions
+        ) : base(dbContextOptions)
+        { }
+        public DbSet<TrekList> TrekLists { get; set; }
+        public DbSet<TrekPackage> TrekPackages { get; set; }
     }
 }
