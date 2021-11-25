a69e4722f6b1fd55568deb67e19d10706ef3d3ec
paolomococci <paolomococci@gmail.com>
2021-11-25 07:04:39 +0100

    I have edited the class TrekPackageRecord of project Biking.Application belonging to solution Biking

--- Biking/src/core/Biking.Application/TrekLists/Queries/ExportTreks/TrekPackageRecord.cs
+++ Biking/src/core/Biking.Application/TrekLists/Queries/ExportTreks/TrekPackageRecord.cs
@@ -1,7 +1,10 @@
+using Biking.Application.Common.Mappings;
+
 namespace Biking.Application.TrekLists.Queries.ExportTreks
 {
-    public class TrekPackageRecord
+    public class TrekPackageRecord : IMapFrom<TrekPackage>
     {
-
+        public string Name { get; set; }
+        public string MapLocation { get; set; }
     }
 }
