84e042e68ce58653c7b20c639ddbaf921ca20d61
paolomococci <paolomococci@gmail.com>
2021-11-25 07:16:43 +0100

    I have edited the class GetTreksQueryHandler of project Biking.Application belonging to solution Biking

--- Biking/src/core/Biking.Application/TrekLists/Queries/GetTreks/GetTreksQueryHandler.cs
+++ Biking/src/core/Biking.Application/TrekLists/Queries/GetTreks/GetTreksQueryHandler.cs
@@ -1,12 +1,39 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Biking.Application.Common.Interfaces;
+using Biking.Application.Dtos.Trek;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Biking.Application.TrekLists.Queries.GetTreks
 {
-    public class GetTreksQueryHandler
+    public class GetTreksQueryHandler : IRequestHandler<GetTreksQuery, TreksVm>
     {
+        private readonly IApplicationDbContext _iApplicationDbContext;
+        private readonly IMapper _iMapper;
 
+        public GetTreksQueryHandler(
+            IApplicationDbContext context,
+            IMapper mapper
+        )
+        {
+            _iApplicationDbContext = context;
+            _iMapper = mapper;
+        }
+
+        public async Task<TreksVm> Handle(GetTreksQuery request, CancellationToken cancellationToken)
+        {
+            return new TreksVm
+            {
+                Lists = await _iApplicationDbContext
+                    .TrekLists
+                    .ProjectTo<TrekListDto>(_iMapper.ConfigurationProvider)
+                    .OrderBy(trekListDto => trekListDto.City)
+                    .ToListAsync(cancellationToken)
+            };
+        }
     }
 }
