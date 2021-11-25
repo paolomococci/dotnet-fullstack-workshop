62f5edc054d6de1ce7da585966cf71d2c037102b
paolomococci <paolomococci@gmail.com>
2021-11-25 16:41:53 +0100

    I have edited the class DependencyInjection of project Biking.Shared

--- Biking/src/infrastructure/Biking.Shared/DependencyInjection.cs
+++ Biking/src/infrastructure/Biking.Shared/DependencyInjection.cs
@@ -1,8 +1,25 @@
+using AutoMapper.Configuration;
+using Biking.Application.Common.Interfaces;
+using Biking.Domain.Settings;
+using Biking.Shared.Files;
+using Biking.Shared.Services;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Biking.Shared
 {
-	public class DependencyInjection
-	{
-		
-	}
-}
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureShared(
+            this IServiceCollection services,
+            IConfiguration config
+        )
+        {
+            services.Configure<MailSettings>(config.GetSection("MailSettings"));
+            services.AddTransient<IDateTime, DateTimeService>();
+            services.AddTransient<IEmailService, EmailService>();
+            services.AddTransient<ICsvFileBuilder, CsvFileBuilder>();
 
+            return services;
+        }
+    }
+}
