c845c2d7194103123b943af98e6b3e1e3b828407
paolomococci <paolomococci@gmail.com>
2021-11-25 07:13:34 +0100

    I have edited the class TreksVm of project Biking.Application belonging to solution Biking

--- Biking/src/core/Biking.Application/TrekLists/Queries/GetTreks/TreksVm.cs
+++ Biking/src/core/Biking.Application/TrekLists/Queries/GetTreks/TreksVm.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
+using Biking.Application.Dtos.Trek;
+
 namespace Biking.Application.TrekLists.Queries.GetTreks
 {
     public class TreksVm
     {
-
+        public IList<TrekListDto> Lists { get; set; }
     }
 }
