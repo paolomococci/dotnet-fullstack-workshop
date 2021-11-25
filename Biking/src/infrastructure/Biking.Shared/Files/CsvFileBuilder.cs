b7d7801f0dd5d6918c0c56c15d3523cc9d622908
paolomococci <paolomococci@gmail.com>
2021-11-25 16:30:16 +0100

    I have edited the class CsvFileBuilder of project Biking.Shared

--- Biking/src/infrastructure/Biking.Shared/Files/CsvFileBuilder.cs
+++ Biking/src/infrastructure/Biking.Shared/Files/CsvFileBuilder.cs
@@ -1,7 +1,30 @@
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using Biking.Application.Common.Interfaces;
+using Biking.Application.TrekLists.Queries.ExportTreks;
+using CsvHelper;
+
 namespace Biking.Shared.Files
 {
-    public class CsvFileBuilder
+    public class CsvFileBuilder : ICsvFileBuilder
     {
+        public byte[] BuildTrekPackagesFile(
+            IEnumerable<TrekPackageRecord> records
+        )
+        {
+            using var memoryStream = new MemoryStream();
 
+            using (var streamWriter = new StreamWriter(memoryStream))
+            {
+                using var csvWriter = new CsvWriter(
+                    streamWriter,
+                    CultureInfo.InvariantCulture
+                );
+                csvWriter.WriteRecords(records);
+            }
+
+            return memoryStream.ToArray();
+        }
     }
 }
