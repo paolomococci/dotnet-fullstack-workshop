c73629c428a2b77f11052f339a63a8a01450bcea
paolomococci <paolomococci@gmail.com>
2021-11-25 09:57:54 +0100

    I have edited the class UpdateTrekPackageCommand of project Biking.Application belonging to solution Biking

--- Biking/src/core/Biking.Application/TrekPackages/Commands/UpdateTrekPackage/UpdateTrekPackageCommand.cs
+++ Biking/src/core/Biking.Application/TrekPackages/Commands/UpdateTrekPackage/UpdateTrekPackageCommand.cs
@@ -1,7 +1,10 @@
+using MediatR;
+
 namespace Biking.Application.TrekPackages.Commands.UpdateTrekPackage
 {
-    public class UpdateTrekPackageCommand
+    public class UpdateTrekPackageCommand : IRequest
     {
-
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
