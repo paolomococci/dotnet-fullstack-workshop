e4f9f6875042fb45b913016fd98682a9de576972
paolomococci <paolomococci@gmail.com>
2021-11-25 07:02:28 +0100

    I have edited the class ExportTreksVm of project Biking.Application belonging to solution Biking

--- Biking/src/core/Biking.Application/TrekLists/Queries/ExportTreks/ExportTreksVm.cs
+++ Biking/src/core/Biking.Application/TrekLists/Queries/ExportTreks/ExportTreksVm.cs
@@ -2,6 +2,8 @@ namespace Biking.Application.TrekLists.Queries.ExportTreks
 {
     public class ExportTreksVm
     {
-
+        public string FileName { get; set; }
+        public string ContentType { get; set; }
+        public byte[] Content { get; set; }
     }
 }
