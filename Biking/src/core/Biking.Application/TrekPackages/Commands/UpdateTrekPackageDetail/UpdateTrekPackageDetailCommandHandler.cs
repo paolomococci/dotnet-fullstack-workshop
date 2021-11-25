1acc09c92be904de89938c01470a12c4d7d1df53
paolomococci <paolomococci@gmail.com>
2021-11-25 10:31:20 +0100

    I have edited the class UpdateTrekPackageDetailCommandHandler of project Biking.Application belonging to solution Biking

--- Biking/src/core/Biking.Application/TrekPackages/Commands/UpdateTrekPackageDetail/UpdateTrekPackageDetailCommandHandler.cs
+++ Biking/src/core/Biking.Application/TrekPackages/Commands/UpdateTrekPackageDetail/UpdateTrekPackageDetailCommandHandler.cs
@@ -1,7 +1,47 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Biking.Application.Common.Exceptions;
+using Biking.Application.Common.Interfaces;
+using MediatR;
+
 namespace Biking.Application.TrekPackages.Commands.UpdateTrekPackageDetail
 {
-    public class UpdateTrekPackageDetailCommandHandler
+    public class UpdateTrekPackageDetailCommandHandler : IRequestHandler<UpdateTrekPackageDetailCommand>
     {
+        private readonly IApplicationDbContext _iApplicationDbContext;
 
+        public UpdateTrekPackageDetailCommandHandler(
+            IApplicationDbContext context
+        )
+        {
+            _iApplicationDbContext = context;
+        }
+
+        public async Task<Unit> Handle(
+            UpdateTrekPackageDetailCommand request,
+            CancellationToken cancellationToken
+        )
+        {
+            var entity = await _iApplicationDbContext.TrekPackages.FindAsync(request.Id);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(TrekPackage), request.Id);
+            }
+
+            entity.Id = request.Id;
+            entity.ListId = request.ListId;
+            entity.Name = request.Name;
+            entity.Hope = request.Hope;
+            entity.MapLocation = request.MapLocation;
+            entity.Price = request.Price;
+            entity.Duration = request.Duration;
+            entity.Confirmation = request.Confirmation;
+            entity.Currency = request.Currency;
+
+            await _iApplicationDbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
     }
 }
