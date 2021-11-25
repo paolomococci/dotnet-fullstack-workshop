8e8a6c3eb033db76b2ea67ad4366cbc9b6655ad4
paolomococci <paolomococci@gmail.com>
2021-11-25 09:43:41 +0100

    I have edited the class DeleteTrekPackageCommand of project Biking.Application belonging to solution Biking

--- Biking/src/core/Biking.Application/TrekPackages/Commands/DeleteTrekPackage/DeleteTrekPackageCommand.cs
+++ Biking/src/core/Biking.Application/TrekPackages/Commands/DeleteTrekPackage/DeleteTrekPackageCommand.cs
@@ -1,7 +1,9 @@
+using MediatR;
+
 namespace Biking.Application.TrekPackages.Commands.DeleteTrekPackage
 {
-    public class DeleteTrekPackageCommand
+    public class DeleteTrekPackageCommand : IRequest
     {
-
+        public int Id { get; set; }
     }
 }
