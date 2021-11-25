5b210cee5d9f2b88901f730306d9ba54b6d61da3
paolomococci <paolomococci@gmail.com>
2021-11-25 10:35:51 +0100

    I have edited the class DependencyInjection of project Biking.Application belonging to solution Biking

--- Biking/src/core/Biking.Application/DependencyInjection.cs
+++ Biking/src/core/Biking.Application/DependencyInjection.cs
@@ -1,8 +1,25 @@
+using System.Reflection;
+using Biking.Application.Common.Behaviors;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Biking.Application
 {
-	public class DependencyInjection
-	{
-		
-	}
-}
+    public class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(
+            this IServiceCollection services
+        )
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehavior<,>));
 
+            return services;
+        }
+    }
+}
