8c87841bc9d39650dab902d7fc865bcd333e2862
paolomococci <paolomococci@gmail.com>
2021-11-26 10:57:19 +0100

    I have edited the DependencyInjection class belonging to the Biking.Identity project

--- Biking/src/infrastructure/Biking.Identity/DependencyInjection.cs
+++ Biking/src/infrastructure/Biking.Identity/DependencyInjection.cs
@@ -1,8 +1,25 @@
+using Biking.Application.Common.Interfaces;
+using Biking.Identity.Helpers;
+using Biking.Identity.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Biking.Identity
 {
-	public class DependencyInjection
-	{
-		
-	}
-}
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureIdentity(
+            this IServiceCollection iServiceCollection,
+            IConfiguration iConfiguration
+        )
+        {
+            iServiceCollection.Configure<AuthSettings>(
+                iConfiguration.GetSection(nameof(AuthSettings))
+            );
 
+            iServiceCollection.AddScoped<IUserService, UserService>();
+
+            return iServiceCollection;
+        }
+    }
+}
