22fc140d465995219b2b85a0c2ec8ec4efd4e8bf
paolomococci <paolomococci@gmail.com>
2021-11-26 22:00:44 +0100

    I have edited the UsersController class belonging to the Biking.WebApi project

--- Biking/src/presentation/Biking.WebApi/Controllers/v1/UsersController.cs
+++ Biking/src/presentation/Biking.WebApi/Controllers/v1/UsersController.cs
@@ -1,7 +1,37 @@
+using Biking.Application.Common.Interfaces;
+using Biking.Application.Dtos.User;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Biking.WebApi.Controllers.v1
 {
-    public class UsersController
+    [ApiVersion("1.0")]
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class UsersController : ControllerBase
     {
+        private readonly IUserService _iUserService;
+        public UsersController(
+            IUserService userService
+        ) => _iUserService = userService;
 
+        [HttpPost("auth")]
+        public IActionResult Authenticate(
+            [FromBody] AuthenticateRequest model
+        )
+        {
+            var response = _iUserService.Authenticate(model);
+
+            if (response == null)
+            {
+                return BadRequest(
+                    new
+                    {
+                        message = "access denied"
+                    }
+                );
+            }
+
+            return Ok(response);
+        }
     }
 }
