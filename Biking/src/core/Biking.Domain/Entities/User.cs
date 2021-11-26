bbb64ab402dbe652a50b1e1cfb739bc44d9a1737
paolomococci <paolomococci@gmail.com>
2021-11-26 07:46:26 +0100

    I have edited the class User of project Biking.Domain

--- Biking/src/core/Biking.Domain/Entities/User.cs
+++ Biking/src/core/Biking.Domain/Entities/User.cs
@@ -1,8 +1,11 @@
 namespace Biking.Domain.Entities
 {
-	public class User
-	{
-		
-	}
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
 }
-
