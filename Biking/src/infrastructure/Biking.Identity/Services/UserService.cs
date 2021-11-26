071b7a285d9859934e0aeb6aeaebcf4bb692d57a
paolomococci <paolomococci@gmail.com>
2021-11-26 09:24:31 +0100

    I started to edit the UserService class belonging to the Biking.Identity project

--- Biking/src/infrastructure/Biking.Identity/Services/UserService.cs
+++ Biking/src/infrastructure/Biking.Identity/Services/UserService.cs
@@ -1,8 +1,20 @@
+using Biking.Application.Common.Interfaces;
+using Biking.Application.Dtos.User;
+using Biking.Domain.Entities;
+
 namespace Biking.Identity.Services
 {
-	public class UserService
-	{
-		
-	}
+    public class UserService : IUserService
+    {
+        public AuthenticateResponse Authenticate(AuthenticateRequest model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public User GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
 }
 
