c17fb67f72720d72ae186f046813f35072dc6dc2
paolomococci <paolomococci@gmail.com>
2021-11-26 09:20:47 +0100

    I have edited the IUserService class belonging to the Biking.Application project

--- Biking/src/core/Biking.Application/Common/Interfaces/IUserService.cs
+++ Biking/src/core/Biking.Application/Common/Interfaces/IUserService.cs
@@ -1,8 +1,11 @@
+using Biking.Application.Dtos.User;
+using Biking.Domain.Entities;
+
 namespace Biking.Application.Common.Interfaces
 {
-	public interface IUserService
-	{
-		
-	}
+    public interface IUserService
+    {
+        AuthenticateResponse Authenticate(AuthenticateRequest model);
+        User GetById(int id);
+    }
 }
-
