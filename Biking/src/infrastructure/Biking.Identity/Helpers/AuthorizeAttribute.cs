8369545389cabdb8a3144547289ac4f446ff893a
paolomococci <paolomococci@gmail.com>
2021-11-26 08:53:13 +0100

    I have edited the AuthorizeAttribute class belonging to the Biking.Identity project

--- Biking/src/infrastructure/Biking.Identity/Helpers/AuthorizeAttribute.cs
+++ Biking/src/infrastructure/Biking.Identity/Helpers/AuthorizeAttribute.cs
@@ -1,4 +1,7 @@
 using System;
+using Biking.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Biking.Identity.Helpers
@@ -6,9 +9,20 @@ namespace Biking.Identity.Helpers
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
     public class AuthorizeAttribute : Attribute, IAuthorizationFilter
     {
-        public void OnAuthorization(AuthorizationFilterContext context)
+        public void OnAuthorization(
+            AuthorizationFilterContext authorizationFilterContext
+        )
         {
-            throw new NotImplementedException();
+            var user = (User)authorizationFilterContext.HttpContext.Items["User"];
+            if (user == null)
+            {
+                authorizationFilterContext.Result = new JsonResult(
+                    new { message = "Unauthorized" }
+                )
+                {
+                    StatusCode = StatusCodes.Status401Unauthorized
+                };
+            }
         }
     }
 }
