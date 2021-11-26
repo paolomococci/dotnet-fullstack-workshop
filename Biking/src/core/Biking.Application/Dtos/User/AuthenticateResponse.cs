eb04431946fb40a8ce05607514e5804bd6a00722
paolomococci <paolomococci@gmail.com>
2021-11-26 09:15:31 +0100

    I have edited the AuthenticateResponse class belonging to the Biking.Application project

--- Biking/src/core/Biking.Application/Dtos/User/AuthenticateResponse.cs
+++ Biking/src/core/Biking.Application/Dtos/User/AuthenticateResponse.cs
@@ -1,8 +1,24 @@
 namespace Biking.Application.Dtos.User
 {
-	public class AuthenticateResponse
-	{
-		
-	}
+    public class AuthenticateResponse
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Token { get; set; }
+
+        public AuthenticateResponse(
+            Biking.Domain.Entities.User user,
+            string token
+        )
+        {
+            Id = user.Id;
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+            Username = user.Username;
+            Token = token;
+        }
+    }
 }
 
