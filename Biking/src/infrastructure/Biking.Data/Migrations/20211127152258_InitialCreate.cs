c99b3ba014203027c9d9ac0f87cd1f36234d7969
paolomococci <paolomococci@gmail.com>
2021-11-27 16:30:53 +0100

    data migration performed in project Biking.Data

---
+++ Biking/src/infrastructure/Biking.Data/Migrations/20211127152258_InitialCreate.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Biking.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TrekLists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Country = table.Column<string>(type: "TEXT", nullable: true),
+                    City = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TrekLists", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TrekPackages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ListId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Hope = table.Column<string>(type: "TEXT", nullable: true),
+                    MapLocation = table.Column<string>(type: "TEXT", nullable: true),
+                    Price = table.Column<float>(type: "REAL", nullable: false),
+                    Duration = table.Column<int>(type: "INTEGER", nullable: false),
+                    Confirmation = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Currency = table.Column<int>(type: "INTEGER", nullable: false),
+                    TrekListId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TrekPackages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TrekPackages_TrekLists_TrekListId",
+                        column: x => x.TrekListId,
+                        principalTable: "TrekLists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TrekPackages_TrekListId",
+                table: "TrekPackages",
+                column: "TrekListId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TrekPackages");
+
+            migrationBuilder.DropTable(
+                name: "TrekLists");
+        }
+    }
+}
