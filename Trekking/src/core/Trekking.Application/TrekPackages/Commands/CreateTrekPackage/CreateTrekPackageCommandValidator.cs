2bdfd4521ef12234ffceb57f287cae4082e7ee11
paolomococci <paolomococci@gmail.com>
2021-11-28 16:39:01 +0100

    I added class CreateTrekPackageCommandValidator to project Trekking.Application

---
+++ Trekking/src/core/Trekking.Application/TrekPackages/Commands/CreateTrekPackage/CreateTrekPackageCommandValidator.cs
@@ -0,0 +1,35 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Trekking.Application.Common.Interfaces;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace Trekking.Application.TrekPackages.Commands.CreateTrekPackage
+{
+    public class CreateTrekPackageCommandValidator : AbstractValidator<CreateTrekPackageCommand>
+    {
+        private readonly IApplicationDbContext _iApplicationDbContext;
+
+        public CreateTrekPackageCommandValidator(IApplicationDbContext context)
+        {
+            _iApplicationDbContext = context;
+
+            RuleFor(createTrekPackageCommand => createTrekPackageCommand.Name)
+                .NotEmpty().WithMessage("Name field is required.")
+                .MaximumLength(128).WithMessage("Name field must not exceed 128 characters.")
+                .MustAsync(NameFieldMustBeUnique).WithMessage("The specified name field already used.");
+
+            RuleFor(createTrekPackageCommand => createTrekPackageCommand.ListId)
+                .NotEmpty().WithMessage("ListId field is required");
+        }
+
+        public async Task<bool> NameFieldMustBeUnique(
+            string name, CancellationToken cancellationToken
+        )
+        {
+            return await _iApplicationDbContext.TrekPackages.AllAsync(
+                trekPackage => trekPackage.Name != name
+            );
+        }
+    }
+}
