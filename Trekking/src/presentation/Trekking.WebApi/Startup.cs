f67bfe257303562b557366972c70aac9d28a7954
paolomococci <paolomococci@gmail.com>
2021-11-28 19:04:51 +0100

    I edit class Startup belonging to the Trekking.WebApi project

--- Trekking/src/presentation/Trekking.WebApi/Startup.cs
+++ Trekking/src/presentation/Trekking.WebApi/Startup.cs
@@ -1,16 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
+
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+using System.Collections.Generic;
+
+using Trekking.Application;
+using Trekking.Data;
+using Trekking.Identity.Helpers;
+using Trekking.Shared;
+using Trekking.WebApi.Filters;
+using Trekking.WebApi.Helpers;
 
 namespace Trekking.WebApi
 {
@@ -26,27 +33,97 @@ namespace Trekking.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplication();
+            services.AddInfrastructureData();
+            services.AddInfrastructureShared(Configuration);
+            services.AddHttpContextAccessor();
+            services.AddControllers();
+
+            services.AddControllersWithViews(
+                options => options.Filters.Add(new ApiExceptionFilter())
+            );
+
+            services.Configure<ApiBehaviorOptions>(
+                options => options.SuppressModelStateInvalidFilter = true
+            );
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
+
+            services.AddSwaggerGen(swaggerUIOptions =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Trekking.WebApi", Version = "v1" });
+                swaggerUIOptions.OperationFilter<SwaggerDefaultValues>();
+                swaggerUIOptions.AddSecurityDefinition(
+                    "Bearer",
+                    new OpenApiSecurityScheme
+                    {
+                        Description = "JWT Authorization header using the Bearer scheme.",
+                        Name = "Authorization",
+                        In = ParameterLocation.Header,
+                        Type = SecuritySchemeType.Http,
+                        Scheme = "bearer"
+                    }
+                );
+                swaggerUIOptions.AddSecurityRequirement(
+                    new OpenApiSecurityRequirement
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            }, new List<string>()
+                        }
+                    }
+                );
+            });
+
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+
+            services.AddApiVersioning(config =>
+            {
+                config.DefaultApiVersion = new ApiVersion(1, 0);
+                config.AssumeDefaultVersionWhenUnspecified = true;
+                config.ReportApiVersions = true;
+            });
+
+            services.AddVersionedApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(
+            IApplicationBuilder app,
+            IWebHostEnvironment env,
+            IApiVersionDescriptionProvider provider
+        )
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Trekking.WebApi v1"));
+                app.UseSwaggerUI(swaggerUIOptions =>
+                {
+                    foreach (var description in provider.ApiVersionDescriptions)
+                    {
+                        swaggerUIOptions.SwaggerEndpoint(
+                            $"/swagger/{description.GroupName}/swagger.json",
+                            description.GroupName.ToUpperInvariant()
+                        );
+                    }
+                });
             }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseMiddleware<JwtMiddleware>();
 
             app.UseAuthorization();
 
