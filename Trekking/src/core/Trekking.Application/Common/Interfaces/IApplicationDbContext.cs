bfecf18685c98b65918fdd6ce1447edf119d29da
paolomococci <paolomococci@gmail.com>
2021-11-28 15:43:39 +0100

    I edit interface IApplicationDbContext belonging to the Trekking.Application project

--- Trekking/src/core/Trekking.Application/Common/Interfaces/IApplicationDbContext.cs
+++ Trekking/src/core/Trekking.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -1,7 +1,16 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Trekking.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace Trekking.Application.Common.Interfaces
 {
-	public interface IApplicationDbContext
-	{
-		
-	}
+    public interface IApplicationDbContext
+    {
+        DbSet<TrekList> TrekLists { get; set; }
+
+        DbSet<TrekPackage> TrekPackages { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
 }
