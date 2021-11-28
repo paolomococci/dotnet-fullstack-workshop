c4854c66e305dc64e01ae75556daaf58d45ed9f4
paolomococci <paolomococci@gmail.com>
2021-11-28 16:44:25 +0100

    I added class UpdateTrekPackageDetailCommandValidator to project Trekking.Application

---
+++ Trekking/src/core/Trekking.Application/TrekPackages/Commands/UpdateTrekPackageDetail/UpdateTrekPackageDetailCommandValidator.cs
@@ -0,0 +1,67 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Trekking.Application.Common.Interfaces;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace Trekking.Application.TrekPackages.Commands.UpdateTrekPackageDetail
+{
+    public class UpdateTrekPackageDetailCommandValidator : AbstractValidator<UpdateTrekPackageDetailCommand>
+    {
+        private readonly IApplicationDbContext _iApplicationDbContext;
+
+        public UpdateTrekPackageDetailCommandValidator(
+            IApplicationDbContext context
+        )
+        {
+            _iApplicationDbContext = context;
+
+            RuleFor(updateTrekPackageDetailCommand => updateTrekPackageDetailCommand.ListId)
+                .NotEmpty().WithMessage("ListId field is required")
+                .MustAsync(ListIdFieldMustBeUnique).WithMessage("The specified identifier of the list already used.");
+
+            RuleFor(updateTrekPackageDetailCommand => updateTrekPackageDetailCommand.Name)
+                .NotEmpty().WithMessage("Name field is required")
+                .MaximumLength(128).WithMessage("Name field must not exceed 128 characters.")
+                .MustAsync(NameFieldMustBeUnique).WithMessage("The specified name field already used.");
+
+            RuleFor(updateTrekPackageDetailCommand => updateTrekPackageDetailCommand.Hope)
+                .MaximumLength(128).WithMessage("Hope field must not exceed 128 characters.")
+                .NotEmpty().WithMessage("Hope field is required");
+
+            RuleFor(updateTrekPackageDetailCommand => updateTrekPackageDetailCommand.MapLocation)
+                .MaximumLength(128).WithMessage("MapLocation field must not exceed 128 characters.")
+                .NotEmpty().WithMessage("MapLocation field is required");
+
+            RuleFor(updateTrekPackageDetailCommand => updateTrekPackageDetailCommand.Price)
+                .NotEmpty().WithMessage("Price field is required");
+
+            RuleFor(updateTrekPackageDetailCommand => updateTrekPackageDetailCommand.Duration)
+                .NotEmpty().WithMessage("Duration field is required");
+
+            RuleFor(updateTrekPackageDetailCommand => updateTrekPackageDetailCommand.Confirmation)
+                .NotEmpty().WithMessage("Confirmation field is required");
+
+            RuleFor(updateTrekPackageDetailCommand => updateTrekPackageDetailCommand.Currency)
+                .NotEmpty().WithMessage("Currency field is required");
+        }
+
+        public async Task<bool> ListIdFieldMustBeUnique(
+            int ListId, CancellationToken cancellationToken
+        )
+        {
+            return await _iApplicationDbContext.TrekPackages.AllAsync(
+                trekPackage => trekPackage.ListId != ListId
+            );
+        }
+
+        public async Task<bool> NameFieldMustBeUnique(
+            string name, CancellationToken cancellationToken
+        )
+        {
+            return await _iApplicationDbContext.TrekPackages.AllAsync(
+                trekPackage => trekPackage.Name != name
+            );
+        }
+    }
+}
