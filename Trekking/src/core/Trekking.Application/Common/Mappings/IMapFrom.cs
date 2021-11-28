57bf2460b9f3b9363aaffc06d57c0d388c2add12
paolomococci <paolomococci@gmail.com>
2021-11-28 15:54:45 +0100

    I edit interface IMapFrom belonging to the Trekking.Application project

--- Trekking/src/core/Trekking.Application/Common/Mappings/IMapFrom.cs
+++ Trekking/src/core/Trekking.Application/Common/Mappings/IMapFrom.cs
@@ -1,7 +1,11 @@
+using AutoMapper;
+
 namespace Trekking.Application.Common.Mappings
 {
-	public interface IMapFrom
-	{
-		
-	}
+    public interface IMapFrom<T>
+    {
+        void Mapping(
+            Profile profile) => profile.CreateMap(typeof(T), GetType()
+        );
+    }
 }
