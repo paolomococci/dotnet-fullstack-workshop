5b28df20470d8934d448ba807e6ca9e856fd3360
paolomococci <paolomococci@gmail.com>
2021-11-28 16:17:47 +0100

    I added class UpdateTrekListCommand to project Trekking.Application

---
+++ Trekking/src/core/Trekking.Application/TrekLists/Commands/UpdateTrekList/UpdateTrekListCommand.cs
@@ -0,0 +1,11 @@
+using MediatR;
+
+namespace Trekking.Application.TrekLists.Commands.UpdateTrekList
+{
+    public class UpdateTrekListCommand : IRequest
+    {
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+    }
+}
