8505e2d2b22da7fb13c449bd97e7bf80c3de55af
paolomococci <paolomococci@gmail.com>
2021-11-28 15:38:26 +0100

    I edit the class NotFoundException belonging to the Trekking.Application project

--- Trekking/src/core/Trekking.Application/Common/Exceptions/NotFoundException.cs
+++ Trekking/src/core/Trekking.Application/Common/Exceptions/NotFoundException.cs
@@ -1,7 +1,17 @@
+using System;
+
 namespace Trekking.Application.Common.Exceptions
 {
-	public class NotFoundException
-	{
-		
-	}
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message) { }
+
+        public NotFoundException(
+            string message, Exception exception
+        ) : base(message, exception) { }
+
+        public NotFoundException(
+            string entityName, object objectKey
+        ) : base($"Entity \"{entityName}\" ({objectKey}) was not found.") { }
+    }
 }
