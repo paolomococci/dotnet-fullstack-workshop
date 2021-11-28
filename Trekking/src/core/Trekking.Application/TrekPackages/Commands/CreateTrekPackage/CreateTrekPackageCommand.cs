42d2f280b8af4caf5ae83c8a9148af3f323574c7
paolomococci <paolomococci@gmail.com>
2021-11-28 16:36:41 +0100

    I added class CreateTrekPackageCommand to project Trekking.Application

---
+++ Trekking/src/core/Trekking.Application/TrekPackages/Commands/CreateTrekPackage/CreateTrekPackageCommand.cs
@@ -0,0 +1,17 @@
+using Trekking.Domain.Enums;
+using MediatR;
+
+namespace Trekking.Application.TrekPackages.Commands.CreateTrekPackage
+{
+    public class CreateTrekPackageCommand : IRequest<int>
+    {
+        public int ListId { get; set; }
+        public string Name { get; set; }
+        public string Hope { get; set; }
+        public string MapLocation { get; set; }
+        public float Price { get; set; }
+        public int Duration { get; set; }
+        public bool Confirmation { get; set; }
+        public Currency Currency { get; set; }
+    }
+}
