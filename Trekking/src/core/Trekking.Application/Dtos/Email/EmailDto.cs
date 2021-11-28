f29afba79122c24de60e38d4f0b23408d8f2f791
paolomococci <paolomococci@gmail.com>
2021-11-28 15:56:23 +0100

    I edit class EmailDto belonging to the Trekking.Application project

--- Trekking/src/core/Trekking.Application/Dtos/Email/EmailDto.cs
+++ Trekking/src/core/Trekking.Application/Dtos/Email/EmailDto.cs
@@ -1,7 +1,10 @@
 namespace Trekking.Application.Dtos.Email
 {
-	public class EmailDto
-	{
-		
-	}
+    public class EmailDto
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string From { get; set; }
+    }
 }
