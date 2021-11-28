71fd0d68211f2b2c9a2af2ad8b140f54c4e420a1
paolomococci <paolomococci@gmail.com>
2021-11-28 10:30:40 +0100

    I edit the class PerformanceBehavior belonging to the Trekking.Application project

--- Trekking/src/core/Trekking.Application/Common/Behaviors/PerformanceBehavior.cs
+++ Trekking/src/core/Trekking.Application/Common/Behaviors/PerformanceBehavior.cs
@@ -1,7 +1,49 @@
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
 namespace Trekking.Application.Common.Behaviors
 {
-	public class PerformanceBehavior
-	{
-		
-	}
+    public class PerformanceBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly Stopwatch _stopwatch;
+        private readonly ILogger<TRequest> _iLogger;
+
+        public PerformanceBehavior(
+            ILogger<TRequest> logger
+        )
+        {
+            _stopwatch = new Stopwatch();
+            _iLogger = logger;
+        }
+
+        public async Task<TResponse> Handle(
+            TRequest tRequest,
+            CancellationToken cancellationToken,
+            RequestHandlerDelegate<TResponse> next
+        )
+        {
+            _stopwatch.Start();
+            var response = await next();
+            _stopwatch.Stop();
+
+            var elapsedMilliseconds = _stopwatch.ElapsedMilliseconds;
+
+            if (elapsedMilliseconds <= 500)
+            {
+                return response;
+            }
+
+            var requestName = typeof(TRequest).Name;
+            _iLogger.LogWarning(
+                "Trekking Long Running Request: {Name} ({ElapsedMilliseconds} milliseconds) {@Request}",
+                requestName,
+                elapsedMilliseconds,
+                tRequest
+            );
+            return response;
+        }
+    }
 }
