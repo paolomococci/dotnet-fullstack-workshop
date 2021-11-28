cdd0dee69c6c6e5c276f3cac78e18f7a63b66ffc
paolomococci <paolomococci@gmail.com>
2021-11-28 10:30:04 +0100

    I edit the class LoggingBehavior belonging to the Trekking.Application project

--- Trekking/src/core/Trekking.Application/Common/Behaviors/LoggingBehavior.cs
+++ Trekking/src/core/Trekking.Application/Common/Behaviors/LoggingBehavior.cs
@@ -1,7 +1,28 @@
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+
 namespace Trekking.Application.Common.Behaviors
 {
-	public class LoggingBehavior
-	{
-		
-	}
+    public class LoggingBehavior<TRequest> : IRequestPreProcessor<TRequest>
+    {
+        private readonly ILogger _iLogger;
+
+        public async Task Process(
+            TRequest tRequest,
+            CancellationToken cancellationToken
+        )
+        {
+            await Task.Run(() =>
+            {
+                var requestName = typeof(TRequest).Name;
+                _iLogger.LogInformation(
+                    "Trekking Request: {@Request}",
+                    requestName,
+                    tRequest
+                );
+            });
+        }
+    }
 }
