edc89d05daa437f7b2f23cb9fc1234ce8b95fcbd
paolomococci <paolomococci@gmail.com>
2021-11-28 19:09:56 +0100

    I added class TrekPackagesController to project Trekking.WebApi

---
+++ Trekking/src/presentation/Trekking.WebApi/Controllers/v1/TrekPackagesController.cs
@@ -0,0 +1,68 @@
+using System.Threading.Tasks;
+using Trekking.Application.TrekPackages.Commands.CreateTrekPackage;
+using Trekking.Application.TrekPackages.Commands.DeleteTrekPackage;
+using Trekking.Application.TrekPackages.Commands.UpdateTrekPackage;
+using Trekking.Application.TrekPackages.Commands.UpdateTrekPackageDetail;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Trekking.WebApi.Controllers.v1
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TrekPackagesController : ApiController
+    {
+        [HttpPost]
+        public async Task<ActionResult<int>> Create(
+            CreateTrekPackageCommand createTrekPackageCommand
+        )
+        {
+            return await Mediator.Send(createTrekPackageCommand);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(
+            int id,
+            UpdateTrekPackageCommand updateTrekPackageCommand
+        )
+        {
+            if (id != updateTrekPackageCommand.Id)
+            {
+                return BadRequest();
+            }
+
+            await Mediator.Send(updateTrekPackageCommand);
+
+            return StatusCode(StatusCodes.Status205ResetContent);
+        }
+
+        [HttpPut("[action]")]
+        public async Task<ActionResult> UpdateItemDetails(
+            int id,
+            UpdateTrekPackageDetailCommand updateTrekPackageDetailCommand
+        )
+        {
+            if (id != updateTrekPackageDetailCommand.Id)
+            {
+                return StatusCode(StatusCodes.Status205ResetContent);
+            }
+
+            await Mediator.Send(updateTrekPackageDetailCommand);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await Mediator.Send(
+                new DeleteTrekPackageCommand
+                {
+                    Id = id
+                }
+            );
+
+            return NoContent();
+        }
+    }
+}
