c5da27dbf83c941006db8d7462aa1d7a9cf4cf79
paolomococci <paolomococci@gmail.com>
2021-11-28 19:06:44 +0100

    I edit class ConfigureSwaggerOptions belonging to the Trekking.WebApi project

--- Trekking/src/presentation/Trekking.WebApi/Helpers/ConfigureSwaggerOptions.cs
+++ Trekking/src/presentation/Trekking.WebApi/Helpers/ConfigureSwaggerOptions.cs
@@ -1,7 +1,55 @@
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
 namespace Trekking.WebApi.Helpers
 {
-	public class ConfigureSwaggerOptions
-	{
-		
-	}
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        private readonly IApiVersionDescriptionProvider _iApiVersionDescriptionProvider;
+
+        public ConfigureSwaggerOptions(
+            IApiVersionDescriptionProvider iApiVersionDescriptionProvider
+        ) => _iApiVersionDescriptionProvider = iApiVersionDescriptionProvider;
+
+        public void Configure(
+            SwaggerGenOptions options
+        )
+        {
+            foreach (var apiVersionDescription in _iApiVersionDescriptionProvider.ApiVersionDescriptions)
+            {
+                options.SwaggerDoc(
+                    apiVersionDescription.GroupName,
+                    OpenApiInfoVersioning(apiVersionDescription)
+                );
+            }
+        }
+
+        private static OpenApiInfo OpenApiInfoVersioning(
+            ApiVersionDescription apiVersionDescription
+        )
+        {
+            var openApiInfo = new OpenApiInfo
+            {
+                Title = "Trekking",
+                Version = apiVersionDescription.ApiVersion.ToString(),
+                Description = "Trekking web API",
+                Contact = new OpenApiContact
+                {
+                    Name = "ICT Department",
+                    Email = "ict@biking.local",
+                    Url = new System.Uri("https://biking.local/ict")
+                }
+            };
+
+            if (apiVersionDescription.IsDeprecated)
+            {
+                openApiInfo.Description += " <em>this API version of Trekking, was deprecated</em>";
+            }
+
+            return openApiInfo;
+        }
+    }
 }
