e31be7d90c4f547a088b8ceb30c444c05876ffbe
paolomococci <paolomococci@gmail.com>
2021-11-28 18:31:25 +0100

    I edit class EmailService belonging to the Trekking.Shared project

--- Trekking/src/infrastructure/Trekking.Shared/Services/EmailService.cs
+++ Trekking/src/infrastructure/Trekking.Shared/Services/EmailService.cs
@@ -1,7 +1,59 @@
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using MailKit.Net.Smtp;
+using Trekking.Application.Common.Exceptions;
+using Trekking.Application.Common.Interfaces;
+using Trekking.Application.Dtos.Email;
+using Trekking.Domain.Settings;
+
 namespace Trekking.Shared.Services
 {
-	public class EmailService
-	{
-		
-	}
+    public class EmailService : IEmailService
+    {
+        private MailSettings MailSettings { get; }
+        private ILogger<EmailService> Logger { get; }
+
+        public EmailService(
+            IOptions<MailSettings> mailSettings,
+            ILogger<EmailService> logger
+        )
+        {
+            MailSettings = mailSettings.Value;
+            Logger = logger;
+        }
+
+        public async Task SendAsync(EmailDto emailRequest)
+        {
+            try
+            {
+                var mimeMessage = new MimeMessage
+                {
+                    Sender = MailboxAddress
+                        .Parse(emailRequest.From ?? MailSettings.EmailFrom)
+                };
+
+                mimeMessage.To.Add(MailboxAddress.Parse(emailRequest.To));
+                mimeMessage.Subject = emailRequest.Subject;
+
+                var builder = new BodyBuilder
+                {
+                    HtmlBody = emailRequest.Body
+                };
+
+                mimeMessage.Body = builder.ToMessageBody();
+
+                using var smtpClient = new SmtpClient();
+
+                await smtpClient.SendAsync(mimeMessage);
+                await smtpClient.DisconnectAsync(true);
+            }
+            catch (System.Exception systemException)
+            {
+                Logger.LogError(systemException.Message, systemException);
+                throw new ApiException(systemException.Message);
+            }
+        }
+    }
 }
