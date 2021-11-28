f8d3e62920d9c4be9cd4d972ad8ff4bbdf3dab38
paolomococci <paolomococci@gmail.com>
2021-11-28 18:20:04 +0100

    I edit class DependencyInjection belonging to the Trekking.Identity project

--- Trekking/src/infrastructure/Trekking.Identity/DependencyInjection.cs
+++ Trekking/src/infrastructure/Trekking.Identity/DependencyInjection.cs
@@ -1,7 +1,25 @@
+using Trekking.Application.Common.Interfaces;
+using Trekking.Identity.Helpers;
+using Trekking.Identity.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Trekking.Identity
 {
-	public class DependencyInjection
-	{
-		
-	}
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureIdentity(
+            this IServiceCollection iServiceCollection,
+            IConfiguration iConfiguration
+        )
+        {
+            iServiceCollection.Configure<AuthSettings>(
+                iConfiguration.GetSection(nameof(AuthSettings))
+            );
+
+            iServiceCollection.AddScoped<IUserService, UserService>();
+
+            return iServiceCollection;
+        }
+    }
 }
