4784bbb77020c89f163547da48ec952a2426200c
paolomococci <paolomococci@gmail.com>
2021-11-28 15:55:28 +0100

    I edit class MappingProfile belonging to the Trekking.Application project

--- Trekking/src/core/Trekking.Application/Common/Mappings/MappingProfile.cs
+++ Trekking/src/core/Trekking.Application/Common/Mappings/MappingProfile.cs
@@ -1,7 +1,31 @@
+using System;
+using System.Linq;
+using System.Reflection;
+using AutoMapper;
+
 namespace Trekking.Application.Common.Mappings
 {
-	public class MappingProfile
-	{
-		
-	}
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            ApplyMappingsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        private void ApplyMappingsFromAssembly(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes().Where(
+                typeT => typeT.GetInterfaces().Any(
+                    typeI => typeI.IsGenericType && typeI.GetGenericTypeDefinition() == typeof(IMapFrom<>)
+                )
+            ).ToList();
+
+            foreach (var typeT in types)
+            {
+                var instance = Activator.CreateInstance(typeT);
+                var methodInfo = typeT.GetMethod("Mapping") ?? typeT.GetInterface("IMapFrom`1").GetMethod("Mapping");
+                methodInfo?.Invoke(instance, new object[] { this });
+            }
+        }
+    }
 }
