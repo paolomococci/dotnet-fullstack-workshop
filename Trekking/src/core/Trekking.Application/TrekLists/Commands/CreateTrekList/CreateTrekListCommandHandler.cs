54cca4b336b6eeab773cdd40db355701ed436fd5
paolomococci <paolomococci@gmail.com>
2021-11-28 16:12:36 +0100

    I added class CreateTrekListCommandHandler to project Trekking.Application

---
+++ Trekking/src/core/Trekking.Application/TrekLists/Commands/CreateTrekList/CreateTrekListCommandHandler.cs
@@ -0,0 +1,35 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Trekking.Application.Common.Interfaces;
+using Trekking.Domain.Entities;
+using MediatR;
+
+namespace Trekking.Application.TrekLists.Commands
+{
+    public class CreateTrekListCommandHandler : IRequestHandler<CreateTrekListCommand, int>
+    {
+        private readonly IApplicationDbContext _iApplicationDbContext;
+
+        public CreateTrekListCommandHandler(IApplicationDbContext context)
+        {
+            _iApplicationDbContext = context;
+        }
+
+        public async Task<int> Handle(
+            CreateTrekListCommand request,
+            CancellationToken cancellationToken
+        )
+        {
+            var trekListEntity = new TrekList
+            {
+                City = request.City
+            };
+
+            _iApplicationDbContext.TrekLists.Add(trekListEntity);
+
+            await _iApplicationDbContext.SaveChangesAsync(cancellationToken);
+
+            return trekListEntity.Id;
+        }
+    }
+}
