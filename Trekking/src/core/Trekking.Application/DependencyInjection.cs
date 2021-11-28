81a1814a9fc46636312daf766ccaf0858f978dfe
paolomococci <paolomococci@gmail.com>
2021-11-28 10:28:57 +0100

    I edit the class DependencyInjection belonging to the Trekking.Application project

--- Trekking/src/core/Trekking.Application/DependencyInjection.cs
+++ Trekking/src/core/Trekking.Application/DependencyInjection.cs
@@ -1,7 +1,25 @@
+using System.Reflection;
+using Trekking.Application.Common.Behaviors;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Trekking.Application
 {
-	public class DependencyInjection
-	{
-		
-	}
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(
+            this IServiceCollection services
+        )
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehavior<,>));
+
+            return services;
+        }
+    }
 }
