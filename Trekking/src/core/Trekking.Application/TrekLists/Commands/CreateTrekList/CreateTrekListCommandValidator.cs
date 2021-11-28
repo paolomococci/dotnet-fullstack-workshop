7db692867c910209f604c4e5de99542afaf3a208
paolomococci <paolomococci@gmail.com>
2021-11-28 16:13:27 +0100

    I added class CreateTrekListCommandValidator to project Trekking.Application

---
+++ Trekking/src/core/Trekking.Application/TrekLists/Commands/CreateTrekList/CreateTrekListCommandValidator.cs
@@ -0,0 +1,23 @@
+using Trekking.Application.Common.Interfaces;
+using FluentValidation;
+
+namespace Trekking.Application.TrekLists.Commands
+{
+    public class CreateTrekListCommandValidator : AbstractValidator<CreateTrekListCommand>
+    {
+        private readonly IApplicationDbContext _iApplicationDbContext;
+
+        public CreateTrekListCommandValidator(IApplicationDbContext context)
+        {
+            _iApplicationDbContext = context;
+
+            RuleFor(createTrekListCommand => createTrekListCommand.Country)
+                .NotEmpty().WithMessage("Country field is required")
+                .MaximumLength(128).WithMessage("The Country field accepts a maximum of 128 characters");
+
+            RuleFor(createTrekListCommand => createTrekListCommand.City)
+                .NotEmpty().WithMessage("City field is required")
+                .MaximumLength(128).WithMessage("The City field accepts a maximum of 128 characters");
+        }
+    }
+}
