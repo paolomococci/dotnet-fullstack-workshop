945ba7a6fcb022cc374041e0aafa6a7d8866d4b4
paolomococci <paolomococci@gmail.com>
2021-11-28 17:13:56 +0100

    I edit class User belonging to the Trekking.Domain project

--- Trekking/src/core/Trekking.Domain/Entities/User.cs
+++ Trekking/src/core/Trekking.Domain/Entities/User.cs
@@ -1,7 +1,11 @@
 namespace Trekking.Domain.Entities
 {
-	public class User
-	{
-		
-	}
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
 }
