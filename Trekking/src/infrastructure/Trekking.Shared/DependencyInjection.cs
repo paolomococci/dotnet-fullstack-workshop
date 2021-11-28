ab51611c216bd5863bb401edcd1e4617abb8b727
paolomococci <paolomococci@gmail.com>
2021-11-28 18:29:50 +0100

    I edit class DependencyInjection belonging to the Trekking.Shared project

--- Trekking/src/infrastructure/Trekking.Shared/DependencyInjection.cs
+++ Trekking/src/infrastructure/Trekking.Shared/DependencyInjection.cs
@@ -1,7 +1,25 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Trekking.Application.Common.Interfaces;
+using Trekking.Domain.Settings;
+using Trekking.Shared.Files;
+using Trekking.Shared.Services;
+
 namespace Trekking.Shared
 {
-	public class DependencyInjection
-	{
-		
-	}
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureShared(
+            this IServiceCollection services,
+            IConfiguration config
+        )
+        {
+            services.Configure<MailSettings>(config.GetSection("MailSettings"));
+            services.AddTransient<IDateTime, DateTimeService>();
+            services.AddTransient<IEmailService, EmailService>();
+            services.AddTransient<ICsvFileBuilder, CsvFileBuilder>();
+
+            return services;
+        }
+    }
 }
