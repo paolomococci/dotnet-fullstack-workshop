a5e5078c515cb30f51d8ac91cce3c4d632c481a7
paolomococci <paolomococci@gmail.com>
2021-11-28 15:35:20 +0100

    I edit the class ApiException belonging to the Trekking.Application project

--- Trekking/src/core/Trekking.Application/Common/Exceptions/ApiException.cs
+++ Trekking/src/core/Trekking.Application/Common/Exceptions/ApiException.cs
@@ -1,7 +1,24 @@
+using System;
+using System.Globalization;
+
 namespace Trekking.Application.Common.Exceptions
 {
-	public class ApiException
-	{
-		
-	}
+    public class ApiException : Exception
+    {
+        public ApiException() : base() { }
+
+        public ApiException(string message) : base(message) { }
+
+        public ApiException(
+            string message,
+            params object[] args
+        ) : base(
+            String.Format(
+                CultureInfo.CurrentCulture,
+                message,
+                args
+            )
+        )
+        { }
+    }
 }
