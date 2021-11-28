cd4889f29e7c75b51820dd358f390a42b2fc9bc1
paolomococci <paolomococci@gmail.com>
2021-11-28 15:52:51 +0100

    I edit interface IUserService belonging to the Trekking.Application project

--- Trekking/src/core/Trekking.Application/Common/Interfaces/IUserService.cs
+++ Trekking/src/core/Trekking.Application/Common/Interfaces/IUserService.cs
@@ -1,7 +1,11 @@
+using Trekking.Application.Dtos.User;
+using Trekking.Domain.Entities;
+
 namespace Trekking.Application.Common.Interfaces
 {
-	public interface IUserService
-	{
-		
-	}
+    public interface IUserService
+    {
+        AuthenticateResponse Authenticate(AuthenticateRequest model);
+        User GetById(int id);
+    }
 }
