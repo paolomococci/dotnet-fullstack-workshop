1dfad4f4526e5224f8f57fe40b7ca50d3ceabc34
paolomococci <paolomococci@gmail.com>
2021-11-28 15:50:22 +0100

    I edit interface IEmailService belonging to the Trekking.Application project

--- Trekking/src/core/Trekking.Application/Common/Interfaces/IEmailService.cs
+++ Trekking/src/core/Trekking.Application/Common/Interfaces/IEmailService.cs
@@ -1,7 +1,10 @@
+using System.Threading.Tasks;
+using Trekking.Application.Dtos.Email;
+
 namespace Trekking.Application.Common.Interfaces
 {
-	public interface IEmailService
-	{
-		
-	}
+    public interface IEmailService
+    {
+        Task SendAsync(EmailDto emailRequest);
+    }
 }
