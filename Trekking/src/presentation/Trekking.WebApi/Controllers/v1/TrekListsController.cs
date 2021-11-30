248bd09412e132f0faed93eb073b402f1bf7e1cf
paolomococci <paolomococci@gmail.com>
2021-11-30 16:51:57 +0100

    I went back to editing the TrekListsController class belonging to the Trekking.WebApi project

--- Trekking/src/presentation/Trekking.WebApi/Controllers/v1/TrekListsController.cs
+++ Trekking/src/presentation/Trekking.WebApi/Controllers/v1/TrekListsController.cs
@@ -9,8 +9,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Trekking.WebApi.Controllers.v1
 {
+    [ApiVersion("1.0")]
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/v{version:apiVersion}/[controller]")]
     public class TrekListsController : ApiController
     {
         [HttpPost]
