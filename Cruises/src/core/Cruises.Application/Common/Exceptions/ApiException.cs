06fc93e5e6f0f8dd318ec44fee0d89e35cdb072e
paolomococci <paolomococci@gmail.com>
2021-12-02 17:15:49 +0100

    class ApiException belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Application/Common/Exceptions/ApiException.cs
+++ Cruises/src/core/Cruises.Application/Common/Exceptions/ApiException.cs
@@ -1,7 +1,25 @@
+using System;
+using System.Globalization;
+
 namespace Cruises.Application.Common.Exceptions
 {
-	public class ApiException
-	{
-		
-	}
+  public class ApiException
+    : Exception
+  {
+    public ApiException() : base() { }
+
+    public ApiException(string message) : base(message) { }
+
+    public ApiException(
+      string message,
+      params object[] args
+    ) : base(
+      String.Format(
+        CultureInfo.CurrentCulture,
+        message,
+        args
+      )
+    )
+    { }
+  }
 }
