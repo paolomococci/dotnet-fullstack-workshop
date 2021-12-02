52a7badfe59f9f8c7701e1a6584eb31818e5e903
paolomococci <paolomococci@gmail.com>
2021-12-02 17:18:53 +0100

    class NotFoundException belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Application/Common/Exceptions/NotFoundException.cs
+++ Cruises/src/core/Cruises.Application/Common/Exceptions/NotFoundException.cs
@@ -1,7 +1,24 @@
+using System;
+
 namespace Cruises.Application.Common.Exceptions
 {
-	public class NotFoundException
-	{
-		
-	}
+  public class NotFoundException
+    : Exception
+  {
+    public NotFoundException(string message)
+      : base(message) { }
+
+    public NotFoundException(
+      string message,
+      Exception exception
+    ) : base(
+      message,
+      exception
+    )
+    { }
+
+    public NotFoundException(
+        string entityName, object objectKey
+    ) : base($"Entity \"{entityName}\" ({objectKey}) was not found.") { }
+  }
 }
