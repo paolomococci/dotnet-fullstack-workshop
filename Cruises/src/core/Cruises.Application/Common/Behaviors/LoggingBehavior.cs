d99b7f3c669445d152d981b18b5b08ac6d890315
paolomococci <paolomococci@gmail.com>
2021-12-02 16:38:23 +0100

    class LoggingBehavior belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Application/Common/Behaviors/LoggingBehavior.cs
+++ Cruises/src/core/Cruises.Application/Common/Behaviors/LoggingBehavior.cs
@@ -1,7 +1,34 @@
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+
 namespace Cruises.Application.Common.Behaviors
 {
-	public class LoggingBehavior
-	{
-		
-	}
+  public class LoggingBehavior<TRequest>
+    : IRequestPreProcessor<TRequest>
+  {
+    private readonly ILogger _iLogger;
+
+    public LoggingBehavior(ILogger<TRequest> logger)
+    {
+      _iLogger = logger;
+    }
+
+    public async Task Process(
+        TRequest tRequest,
+        CancellationToken cancellationToken
+    )
+    {
+      await Task.Run(() =>
+      {
+        var requestName = typeof(TRequest).Name;
+        _iLogger.LogInformation(
+          "Trekking Request: {@Request}",
+          requestName,
+          tRequest
+        );
+      });
+    }
+  }
 }
