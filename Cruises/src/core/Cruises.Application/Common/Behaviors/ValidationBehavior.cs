892eceab363587e65f32f5b7ccf0d6ae379fe711
paolomococci <paolomococci@gmail.com>
2021-12-02 17:08:59 +0100

    class ValidationBehavior belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Application/Common/Behaviors/ValidationBehavior.cs
+++ Cruises/src/core/Cruises.Application/Common/Behaviors/ValidationBehavior.cs
@@ -1,7 +1,55 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Cruises.Application.Common.Exceptions;
+using FluentValidation;
+using MediatR;
+
 namespace Cruises.Application.Common.Behaviors
 {
-	public class ValidationBehavior
-	{
-		
-	}
+  public class ValidationBehavior<TRequest, TResponse>
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+  {
+    private readonly IEnumerable<IValidator<TRequest>> _iEnumerableIValidatorTRequest;
+
+    public ValidationBehavior(
+      IEnumerable<IValidator<TRequest>> validators
+    )
+    {
+      _iEnumerableIValidatorTRequest = validators;
+    }
+
+    public async Task<TResponse> Handle(
+      TRequest request,
+      CancellationToken cancellationToken,
+      RequestHandlerDelegate<TResponse> next
+    )
+    {
+      if (!_iEnumerableIValidatorTRequest.Any())
+      {
+        return await next();
+      }
+
+      var context = new ValidationContext<TRequest>(request);
+
+      var validationResults = await Task.WhenAll(
+        _iEnumerableIValidatorTRequest.Select(
+          validator => validator.ValidateAsync(context, cancellationToken)
+        )
+      );
+
+      var failures = validationResults
+        .SelectMany(validationResult => validationResult.Errors)
+        .Where(validationFailure => validationFailure != null).ToList();
+
+      if (failures.Count != 0)
+      {
+        throw new PropertyValidationException(failures);
+      }
+
+      return await next();
+    }
+  }
 }
