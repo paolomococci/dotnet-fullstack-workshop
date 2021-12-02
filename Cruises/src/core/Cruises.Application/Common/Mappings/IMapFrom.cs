33d64eefb256351dfdf402060a6cee9ee50fc33a
paolomococci <paolomococci@gmail.com>
2021-12-02 18:32:43 +0100

    interface IMapFrom belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Application/Common/Mappings/IMapFrom.cs
+++ Cruises/src/core/Cruises.Application/Common/Mappings/IMapFrom.cs
@@ -1,7 +1,11 @@
+using AutoMapper;
+
 namespace Cruises.Application.Common.Mappings
 {
-	public interface IMapFrom
-	{
-		
-	}
+  public interface IMapFrom<T>
+  {
+    void Mapping(
+        Profile profile) => profile.CreateMap(typeof(T), GetType()
+    );
+  }
 }
