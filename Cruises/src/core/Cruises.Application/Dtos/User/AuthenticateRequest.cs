24a781dedad178bd880fa40eea07af27a1749977
paolomococci <paolomococci@gmail.com>
2021-12-02 18:37:54 +0100

    class AuthenticateRequest belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Application/Dtos/User/AuthenticateRequest.cs
+++ Cruises/src/core/Cruises.Application/Dtos/User/AuthenticateRequest.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Cruises.Application.Dtos.User
 {
-	public class AuthenticateRequest
-	{
-		
-	}
+  public class AuthenticateRequest
+  {
+    [Required]
+    public string Username { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+  }
 }
