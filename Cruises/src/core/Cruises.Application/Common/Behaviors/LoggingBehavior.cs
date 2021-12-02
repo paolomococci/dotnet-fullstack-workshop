7556b98f0a56939e6cf681f04928103b5639b1ab
paolomococci <paolomococci@gmail.com>
2021-12-02 16:42:27 +0100

    edited class LoggingBehavior belonging to project Cruises.Application again

--- Cruises/src/core/Cruises.Application/Common/Behaviors/LoggingBehavior.cs
+++ Cruises/src/core/Cruises.Application/Common/Behaviors/LoggingBehavior.cs
@@ -24,7 +24,7 @@ namespace Cruises.Application.Common.Behaviors
       {
         var requestName = typeof(TRequest).Name;
         _iLogger.LogInformation(
-          "Trekking Request: {@Request}",
+          "Cruises Request: {@Request}",
           requestName,
           tRequest
         );
