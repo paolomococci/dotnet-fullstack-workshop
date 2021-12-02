1d10e6507b1f8dd26ecf94def8357a03183c45da
paolomococci <paolomococci@gmail.com>
2021-12-02 18:24:48 +0100

    interface IUserService belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Application/Common/Interfaces/IUserService.cs
+++ Cruises/src/core/Cruises.Application/Common/Interfaces/IUserService.cs
@@ -1,7 +1,11 @@
+using Cruises.Application.Dtos.User;
+using Cruises.Domain.Entities;
+
 namespace Cruises.Application.Common.Interfaces
 {
-	public interface IUserService
-	{
-		
-	}
+  public interface IUserService
+  {
+    AuthenticateResponse Authenticate(AuthenticateRequest model);
+    User GetById(int id);
+  }
 }
