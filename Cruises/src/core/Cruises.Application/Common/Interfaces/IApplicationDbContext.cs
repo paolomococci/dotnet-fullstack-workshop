e7efe956e5469bddfa7ca14247fd8da98875f860
paolomococci <paolomococci@gmail.com>
2021-12-03 13:27:31 +0100

    interface IApplicationDbContext belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Application/Common/Interfaces/IApplicationDbContext.cs
+++ Cruises/src/core/Cruises.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -1,7 +1,18 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Cruises.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace Cruises.Application.Common.Interfaces
 {
-	public interface IApplicationDbContext
-	{
-		
-	}
+  public interface IApplicationDbContext
+  {
+    DbSet<CruiseList> CruiseLists { get; set; }
+
+    DbSet<CruisePackage> CruisePackages { get; set; }
+
+    Task<int> SaveChangesAsync(
+      CancellationToken cancellationToken
+    );
+  }
 }
