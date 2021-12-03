4cfab2de059d6419f78caeb2f97ae73854b3584a
paolomococci <paolomococci@gmail.com>
2021-12-03 13:26:47 +0100

    class CruisePackageDto belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Application/Dtos/Cruise/CruisePackageDto.cs
+++ Cruises/src/core/Cruises.Application/Dtos/Cruise/CruisePackageDto.cs
@@ -1,7 +1,33 @@
+using AutoMapper;
+using Cruises.Application.Common.Mappings;
+using Cruises.Domain.Entities;
+using Cruises.Domain.Enums;
+
 namespace Cruises.Application.Dtos.Cruise
 {
   public class CruisePackageDto
+    : IMapFrom<CruisePackage>
   {
+    public int Id { get; set; }
+    public int ListId { get; set; }
+    public string Name { get; set; }
+    public string Hope { get; set; }
+    public string MapLocation { get; set; }
+    public float Price { get; set; }
+    public int Duration { get; set; }
+    public bool Confirmation { get; set; }
+    public Currency Currency { get; set; }
 
+    public void Mapping(Profile profile)
+    {
+      profile.CreateMap<CruisePackage, CruisePackageDto>()
+      .ForMember(
+        cruisePackageDto => cruisePackageDto.Currency,
+        iMemberConfigurationExpression => iMemberConfigurationExpression
+          .MapFrom(
+            cruisePackage => (int)cruisePackage.Currency
+          )
+      );
+    }
   }
 }
