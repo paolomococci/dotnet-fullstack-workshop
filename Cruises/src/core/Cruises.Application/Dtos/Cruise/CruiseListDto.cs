a50728ad48d5668d9af4cd87b2ba97b55a9a0fed
paolomococci <paolomococci@gmail.com>
2021-12-03 13:26:05 +0100

    class CruiseListDto belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Application/Dtos/Cruise/CruiseListDto.cs
+++ Cruises/src/core/Cruises.Application/Dtos/Cruise/CruiseListDto.cs
@@ -1,7 +1,20 @@
+using System.Collections.Generic;
+using Cruises.Application.Common.Mappings;
+using Cruises.Domain.Entities;
+
 namespace Cruises.Application.Dtos.Cruise
 {
   public class CruiseListDto
+    : IMapFrom<CruiseList>
   {
+    public IList<CruisePackageDto> Items { get; set; }
+    public int Id { get; set; }
+    public string Country { get; set; }
+    public string City { get; set; }
 
+    public CruiseListDto()
+    {
+      Items = new List<CruisePackageDto>();
+    }
   }
 }
