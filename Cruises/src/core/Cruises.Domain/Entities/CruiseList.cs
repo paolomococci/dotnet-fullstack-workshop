923407ae053dcc7007472c7b853e945dbd774253
paolomococci <paolomococci@gmail.com>
2021-12-03 13:24:02 +0100

    class CruiseList belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Domain/Entities/CruiseList.cs
+++ Cruises/src/core/Cruises.Domain/Entities/CruiseList.cs
@@ -1,7 +1,17 @@
+using System.Collections.Generic;
+
 namespace Cruises.Domain.Entities
 {
   public class CruiseList
   {
+    public CruiseList()
+    {
+      CruisePackages = new List<CruisePackage>();
+    }
 
+    public IList<CruisePackage> CruisePackages { get; set; }
+    public int Id { get; set; }
+    public string Country { get; set; }
+    public string City { get; set; }
   }
 }
