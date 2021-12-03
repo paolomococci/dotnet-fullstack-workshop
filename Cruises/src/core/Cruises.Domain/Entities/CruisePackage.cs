75233c5b20e0575b767b6d3a769c48c214d94048
paolomococci <paolomococci@gmail.com>
2021-12-03 13:25:12 +0100

    class CruisePackage belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Domain/Entities/CruisePackage.cs
+++ Cruises/src/core/Cruises.Domain/Entities/CruisePackage.cs
@@ -1,7 +1,18 @@
+using Cruises.Domain.Enums;
+
 namespace Cruises.Domain.Entities
 {
   public class CruisePackage
   {
-
+    public int Id { get; set; }
+    public int ListId { get; set; }
+    public string Name { get; set; }
+    public string Hope { get; set; }
+    public string MapLocation { get; set; }
+    public float Price { get; set; }
+    public int Duration { get; set; }
+    public bool Confirmation { get; set; }
+    public Currency Currency { get; set; }
+    public CruiseList CruiseList { get; set; }
   }
 }
