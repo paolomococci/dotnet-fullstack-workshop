c72d12becd3e31fe5e506b63b7eea126d4794293
paolomococci <paolomococci@gmail.com>
2021-12-04 06:51:58 +0100

    class CruisesVm belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Application/CruiseLists/Queries/GetCruises/CruisesVm.cs
+++ Cruises/src/core/Cruises.Application/CruiseLists/Queries/GetCruises/CruisesVm.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
+using Cruises.Application.Dtos.Cruise;
+
 namespace Cruises.Application.CruiseLists.Queries.GetCruises
 {
   public class CruisesVm
   {
-
+    public IList<CruiseListDto> Lists { get; set; }
   }
 }
