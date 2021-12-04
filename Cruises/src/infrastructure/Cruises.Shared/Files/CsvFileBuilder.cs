fc65e65e83e840021bc6fec20bc1f8747e94847c
paolomococci <paolomococci@gmail.com>
2021-12-04 19:46:19 +0100

    class CsvFileBuilder belonging to project Cruises.Shared has been edited

--- Cruises/src/infrastructure/Cruises.Shared/Files/CsvFileBuilder.cs
+++ Cruises/src/infrastructure/Cruises.Shared/Files/CsvFileBuilder.cs
@@ -1,7 +1,31 @@
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using Cruises.Application.Common.Interfaces;
+using Cruises.Application.CruiseLists.Queries.ExportCruises;
+using CsvHelper;
+
 namespace Cruises.Shared.Files
 {
-	public class CsvFileBuilder
-	{
-		
-	}
+  public class CsvFileBuilder
+    : ICsvFileBuilder
+  {
+    public byte[] BuildCruisePackagesFile(
+      IEnumerable<CruisePackageRecord> records
+    )
+    {
+      using var memoryStream = new MemoryStream();
+
+      using (var streamWriter = new StreamWriter(memoryStream))
+      {
+        using var csvWriter = new CsvWriter(
+            streamWriter,
+            CultureInfo.InvariantCulture
+        );
+        csvWriter.WriteRecords(records);
+      }
+
+      return memoryStream.ToArray();
+    }
+  }
 }
