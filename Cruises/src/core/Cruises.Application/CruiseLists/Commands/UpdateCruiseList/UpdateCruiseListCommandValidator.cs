ebf0a6de080b03e1aab5633b7be39f5eb3290505
paolomococci <paolomococci@gmail.com>
2021-12-04 17:40:16 +0100

    class UpdateCruiseListCommandValidator belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Application/CruiseLists/Commands/UpdateCruiseList/UpdateCruiseListCommandValidator.cs
+++ Cruises/src/core/Cruises.Application/CruiseLists/Commands/UpdateCruiseList/UpdateCruiseListCommandValidator.cs
@@ -1,7 +1,28 @@
+using Cruises.Application.Common.Interfaces;
+using FluentValidation;
+
 namespace Cruises.Application.CruiseLists.Commands.UpdateCruiseList
 {
   public class UpdateCruiseListCommandValidator
+    : AbstractValidator<UpdateCruiseListCommand>
   {
+    private readonly IApplicationDbContext _iApplicationDbContext;
 
+    public UpdateCruiseListCommandValidator(IApplicationDbContext context)
+    {
+      _iApplicationDbContext = context;
+
+      RuleFor(updateCruiseListCommand => updateCruiseListCommand.Country)
+        .NotEmpty().WithMessage("Country field is required")
+        .MaximumLength(128).WithMessage(
+          "The Country field accepts a maximum of 128 characters"
+        );
+
+      RuleFor(updateCruiseListCommand => updateCruiseListCommand.City)
+        .NotEmpty().WithMessage("City field is required")
+        .MaximumLength(128).WithMessage(
+          "The City field accepts a maximum of 128 characters"
+        );
+    }
   }
 }
