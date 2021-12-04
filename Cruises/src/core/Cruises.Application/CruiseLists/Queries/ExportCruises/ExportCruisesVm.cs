1f99b854e95483c9bb27ac17ac5ffe5feb1608e3
paolomococci <paolomococci@gmail.com>
2021-12-04 11:25:27 +0100

    class ExportCruisesVm belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Application/CruiseLists/Queries/ExportCruises/ExportCruisesVm.cs
+++ Cruises/src/core/Cruises.Application/CruiseLists/Queries/ExportCruises/ExportCruisesVm.cs
@@ -2,6 +2,8 @@ namespace Cruises.Application.CruiseLists.Queries.ExportCruises
 {
   public class ExportCruisesVm
   {
-
+    public string FileName { get; set; }
+    public string ContentType { get; set; }
+    public byte[] Content { get; set; }
   }
 }
