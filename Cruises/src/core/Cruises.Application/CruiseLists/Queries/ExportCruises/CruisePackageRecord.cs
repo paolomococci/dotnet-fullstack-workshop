d4b12f9153c32402d378896ac707c5f2f18ff9b1
paolomococci <paolomococci@gmail.com>
2021-12-04 11:21:28 +0100

    class CruisePackageRecord belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Application/CruiseLists/Queries/ExportCruises/CruisePackageRecord.cs
+++ Cruises/src/core/Cruises.Application/CruiseLists/Queries/ExportCruises/CruisePackageRecord.cs
@@ -1,7 +1,12 @@
+using Cruises.Application.Common.Mappings;
+using Cruises.Domain.Entities;
+
 namespace Cruises.Application.CruiseLists.Queries.ExportCruises
 {
   public class CruisePackageRecord
+    : IMapFrom<CruisePackage>
   {
-
+    public string Name { get; set; }
+    public string MapLocation { get; set; }
   }
 }
