35a577cf6d6f7770df5ccb8a308ff2202d2e8352
paolomococci <paolomococci@gmail.com>
2021-12-04 19:38:34 +0100

    edited class DependencyInjection belonging to project Cruises.Identity again

--- Cruises/src/infrastructure/Cruises.Identity/DependencyInjection.cs
+++ Cruises/src/infrastructure/Cruises.Identity/DependencyInjection.cs
@@ -1,7 +1,25 @@
+using Cruises.Application.Common.Interfaces;
+using Cruises.Identity.Helpers;
+using Cruises.Identity.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Cruises.Identity
 {
   public static class DependencyInjection
   {
+    public static IServiceCollection AddInfrastructureIdentity(
+      this IServiceCollection iServiceCollection,
+      IConfiguration iConfiguration
+    )
+    {
+      iServiceCollection.Configure<AuthSettings>(
+          iConfiguration.GetSection(nameof(AuthSettings))
+      );
 
+      iServiceCollection.AddScoped<IUserService, UserService>();
+
+      return iServiceCollection;
+    }
   }
 }
