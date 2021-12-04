ae98bb2586786c6790bcba3bf075d0c162902593
paolomococci <paolomococci@gmail.com>
2021-12-04 19:50:31 +0100

    class EmailService belonging to project Cruises.Shared has been edited

--- Cruises/src/infrastructure/Cruises.Shared/Services/EmailService.cs
+++ Cruises/src/infrastructure/Cruises.Shared/Services/EmailService.cs
@@ -1,7 +1,60 @@
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using MailKit.Net.Smtp;
+using Cruises.Application.Common.Exceptions;
+using Cruises.Application.Common.Interfaces;
+using Cruises.Application.Dtos.Email;
+using Cruises.Domain.Settings;
+
 namespace Cruises.Shared.Services
 {
-	public class EmailService
-	{
-		
-	}
+  public class EmailService
+    : IEmailService
+  {
+    private MailSettings MailSettings { get; }
+    private ILogger<EmailService> Logger { get; }
+
+    public EmailService(
+      IOptions<MailSettings> mailSettings,
+      ILogger<EmailService> logger
+    )
+    {
+      MailSettings = mailSettings.Value;
+      Logger = logger;
+    }
+
+    public async Task SendAsync(EmailDto emailRequest)
+    {
+      try
+      {
+        var mimeMessage = new MimeMessage
+        {
+          Sender = MailboxAddress
+                .Parse(emailRequest.From ?? MailSettings.EmailFrom)
+        };
+
+        mimeMessage.To.Add(MailboxAddress.Parse(emailRequest.To));
+        mimeMessage.Subject = emailRequest.Subject;
+
+        var builder = new BodyBuilder
+        {
+          HtmlBody = emailRequest.Body
+        };
+
+        mimeMessage.Body = builder.ToMessageBody();
+
+        using var smtpClient = new SmtpClient();
+
+        await smtpClient.SendAsync(mimeMessage);
+        await smtpClient.DisconnectAsync(true);
+      }
+      catch (System.Exception systemException)
+      {
+        Logger.LogError(systemException.Message, systemException);
+        throw new ApiException(systemException.Message);
+      }
+    }
+  }
 }
