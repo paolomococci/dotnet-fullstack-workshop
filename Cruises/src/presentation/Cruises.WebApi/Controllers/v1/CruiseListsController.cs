0b456326e21112294e7ebc33eaa32df293b4460a
paolomococci <paolomococci@gmail.com>
2021-12-04 20:21:03 +0100

    added class CruiseListsController belonging to project Cruises.WebApi

---
+++ Cruises/src/presentation/Cruises.WebApi/Controllers/v1/CruiseListsController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cruises.WebApi.Controllers.v1
+{
+    public class CruiseListsController
+    {
+        
+    }
+}
