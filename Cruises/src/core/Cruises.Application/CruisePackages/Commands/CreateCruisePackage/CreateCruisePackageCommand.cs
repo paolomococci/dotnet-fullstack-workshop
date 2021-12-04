818baf977dae645898d72380589f041548ad25a6
paolomococci <paolomococci@gmail.com>
2021-12-04 06:20:31 +0100

    class CreateCruisePackageCommand belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Application/CruisePackages/Commands/CreateCruisePackage/CreateCruisePackageCommand.cs
+++ Cruises/src/core/Cruises.Application/CruisePackages/Commands/CreateCruisePackage/CreateCruisePackageCommand.cs
@@ -1,7 +1,18 @@
+using Cruises.Domain.Enums;
+using MediatR;
+
 namespace Cruises.Application.CruisePackages.Commands.CreateCruisePackage
 {
   public class CreateCruisePackageCommand
+    : IRequest<int>
   {
-
+    public int ListId { get; set; }
+    public string Name { get; set; }
+    public string Hope { get; set; }
+    public string MapLocation { get; set; }
+    public float Price { get; set; }
+    public int Duration { get; set; }
+    public bool Confirmation { get; set; }
+    public Currency Currency { get; set; }
   }
 }
