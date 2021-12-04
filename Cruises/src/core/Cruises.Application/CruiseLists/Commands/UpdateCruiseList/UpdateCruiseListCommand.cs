d35ba855c93ac99fa3eddb822c90c0c2999c6471
paolomococci <paolomococci@gmail.com>
2021-12-04 11:42:17 +0100

    class UpdateCruiseListCommand belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Application/CruiseLists/Commands/UpdateCruiseList/UpdateCruiseListCommand.cs
+++ Cruises/src/core/Cruises.Application/CruiseLists/Commands/UpdateCruiseList/UpdateCruiseListCommand.cs
@@ -1,7 +1,12 @@
+using MediatR;
+
 namespace Cruises.Application.CruiseLists.Commands.UpdateCruiseList
 {
   public class UpdateCruiseListCommand
+     : IRequest
   {
-
+    public int Id { get; set; }
+    public string Country { get; set; }
+    public string City { get; set; }
   }
 }
