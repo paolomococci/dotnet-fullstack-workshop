084c465b1b61773d57656a8fa4b542b3e383e0e3
paolomococci <paolomococci@gmail.com>
2021-12-04 18:41:10 +0100

    class ApplicationDbContext belonging to project Cruises.Data has been edited

--- Cruises/src/infrastructure/Cruises.Data/Contexts/ApplicationDbContext.cs
+++ Cruises/src/infrastructure/Cruises.Data/Contexts/ApplicationDbContext.cs
@@ -1,7 +1,108 @@
+using System.Data;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Cruises.Application.Common.Interfaces;
+using Cruises.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
 namespace Cruises.Data.Contexts
 {
-	public class ApplicationDbContext
-	{
-		
-	}
+  public class ApplicationDbContext
+    : DbContext, IApplicationDbContext
+  {
+
+    private readonly IDateTime _iDateTime;
+    private IDbContextTransaction _iDbContextTransaction;
+
+    public ApplicationDbContext(
+      DbContextOptions<ApplicationDbContext> options
+    ) : base(options)
+    { }
+
+    public DbSet<CruiseList> CruiseLists
+    {
+      get;
+      set;
+    }
+    public DbSet<CruisePackage> CruisePackages
+    {
+      get;
+      set;
+    }
+
+    public ApplicationDbContext(
+      DbContextOptions<ApplicationDbContext> options,
+      IDateTime dateTime
+    ) : base(options)
+    {
+      _iDateTime = dateTime;
+    }
+
+    public override Task<int> SaveChangesAsync(
+      CancellationToken cancellationToken = new CancellationToken()
+    )
+    {
+      return base.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task BeginTransactionAsync()
+    {
+      if (_iDbContextTransaction != null)
+      {
+        return;
+      }
+
+      _iDbContextTransaction = await base.Database
+          .BeginTransactionAsync(IsolationLevel.ReadCommitted)
+          .ConfigureAwait(false);
+    }
+
+    public async Task CommitTransactionAsync()
+    {
+      try
+      {
+        await SaveChangesAsync().ConfigureAwait(false);
+        _iDbContextTransaction?.Commit();
+      }
+      catch
+      {
+        RollbackTransaction();
+        throw;
+      }
+      finally
+      {
+        if (_iDbContextTransaction != null)
+        {
+          _iDbContextTransaction.Dispose();
+          _iDbContextTransaction = null;
+        }
+      }
+    }
+
+    private void RollbackTransaction()
+    {
+      try
+      {
+        _iDbContextTransaction?.Rollback();
+      }
+      finally
+      {
+        if (_iDbContextTransaction != null)
+        {
+          _iDbContextTransaction.Dispose();
+          _iDbContextTransaction = null;
+        }
+      }
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.ApplyConfigurationsFromAssembly(
+        Assembly.GetExecutingAssembly()
+      );
+      base.OnModelCreating(builder);
+    }
+  }
 }
