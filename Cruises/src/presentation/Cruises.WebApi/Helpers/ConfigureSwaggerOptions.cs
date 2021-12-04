2e36489897b66c6bff7e9654d5b03c5d3b4a0cec
paolomococci <paolomococci@gmail.com>
2021-12-04 20:00:51 +0100

    class ConfigureSwaggerOptions belonging to project Cruises.WebApi has been edited

--- Cruises/src/presentation/Cruises.WebApi/Helpers/ConfigureSwaggerOptions.cs
+++ Cruises/src/presentation/Cruises.WebApi/Helpers/ConfigureSwaggerOptions.cs
@@ -1,7 +1,58 @@
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
 namespace Cruises.WebApi.Helpers
 {
-	public class ConfigureSwaggerOptions
-	{
-		
-	}
+  public class ConfigureSwaggerOptions
+    : IConfigureOptions<SwaggerGenOptions>
+  {
+    private readonly IApiVersionDescriptionProvider _iApiVersionDescriptionProvider;
+
+    public ConfigureSwaggerOptions(
+      IApiVersionDescriptionProvider iApiVersionDescriptionProvider
+    ) => _iApiVersionDescriptionProvider = iApiVersionDescriptionProvider;
+
+    public void Configure(
+      SwaggerGenOptions options
+    )
+    {
+      foreach (
+        var apiVersionDescription in _iApiVersionDescriptionProvider.ApiVersionDescriptions
+      )
+      {
+        options.SwaggerDoc(
+          apiVersionDescription.GroupName,
+          OpenApiInfoVersioning(apiVersionDescription)
+        );
+      }
+    }
+
+    private static OpenApiInfo OpenApiInfoVersioning(
+      ApiVersionDescription apiVersionDescription
+    )
+    {
+      var openApiInfo = new OpenApiInfo
+      {
+        Title = "Cruises",
+        Version = apiVersionDescription.ApiVersion.ToString(),
+        Description = "Cruises web API",
+        Contact = new OpenApiContact
+        {
+          Name = "ICT Department",
+          Email = "ict@biking.local",
+          Url = new System.Uri("https://biking.local/ict")
+        }
+      };
+
+      if (apiVersionDescription.IsDeprecated)
+      {
+        openApiInfo.Description += " <em>this API version of Cruises, was deprecated</em>";
+      }
+
+      return openApiInfo;
+    }
+  }
 }
