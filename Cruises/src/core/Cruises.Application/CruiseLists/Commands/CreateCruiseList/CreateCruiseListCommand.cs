e34ae976551b72d1efd4bce63742502dd91443bd
paolomococci <paolomococci@gmail.com>
2021-12-04 11:28:15 +0100

    class CreateCruiseListCommand belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Application/CruiseLists/Commands/CreateCruiseList/CreateCruiseListCommand.cs
+++ Cruises/src/core/Cruises.Application/CruiseLists/Commands/CreateCruiseList/CreateCruiseListCommand.cs
@@ -1,7 +1,11 @@
+using MediatR;
+
 namespace Cruises.Application.CruiseLists.Commands.CreateCruiseList
 {
   public class CreateCruiseListCommand
+    : IRequest<int>
   {
-
+    public string Country { get; set; }
+    public string City { get; set; }
   }
 }
