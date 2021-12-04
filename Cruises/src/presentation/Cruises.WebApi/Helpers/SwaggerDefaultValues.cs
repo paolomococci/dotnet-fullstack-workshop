47d3c49fbde175643658dbdf371ee9e3d2661889
paolomococci <paolomococci@gmail.com>
2021-12-04 20:02:17 +0100

    class SwaggerDefaultValues belonging to project Cruises.WebApi has been edited

--- Cruises/src/presentation/Cruises.WebApi/Helpers/SwaggerDefaultValues.cs
+++ Cruises/src/presentation/Cruises.WebApi/Helpers/SwaggerDefaultValues.cs
@@ -1,7 +1,46 @@
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
 namespace Cruises.WebApi.Helpers
 {
-	public class SwaggerDefaultValues
-	{
-		
-	}
+  public class SwaggerDefaultValues
+    : IOperationFilter
+  {
+    public void Apply(
+      OpenApiOperation operation,
+      OperationFilterContext context
+    )
+    {
+      var contextApiDescription = context.ApiDescription;
+      operation.Deprecated |= contextApiDescription.IsDeprecated();
+
+      if (operation.Parameters == null)
+      {
+        return;
+      }
+
+      foreach (var parameter in operation.Parameters)
+      {
+        var description = contextApiDescription.ParameterDescriptions.First(
+          apiParameterDescription => apiParameterDescription.Name == parameter.Name
+        );
+
+        parameter.Description ??= description.ModelMetadata.Description;
+
+        if (
+          parameter.Schema.Default == null && description.DefaultValue != null
+        )
+        {
+          parameter.Schema.Default = new OpenApiString(
+            description.DefaultValue.ToString()
+          );
+        }
+
+        parameter.Required |= description.IsRequired;
+      }
+    }
+  }
 }
