5b7aeef99714efc8a2de618494ee88b3a2753b81
paolomococci <paolomococci@gmail.com>
2021-12-04 06:05:26 +0100

    class UpdateCruisePackageCommandHandler belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Application/CruisePackages/Commands/UpdateCruisePackage/UpdateCruisePackageCommandHandler.cs
+++ Cruises/src/core/Cruises.Application/CruisePackages/Commands/UpdateCruisePackage/UpdateCruisePackageCommandHandler.cs
@@ -1,7 +1,41 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Cruises.Application.Common.Exceptions;
+using Cruises.Application.Common.Interfaces;
+using Cruises.Domain.Entities;
+using MediatR;
+
 namespace Cruises.Application.CruisePackages.Commands.UpdateCruisePackage
 {
   public class UpdateCruisePackageCommandHandler
+    : IRequestHandler<UpdateCruisePackageCommand>
   {
+    private readonly IApplicationDbContext _iApplicationDbContext;
 
+    public UpdateCruisePackageCommandHandler(
+      IApplicationDbContext context
+    )
+    {
+      _iApplicationDbContext = context;
+    }
+
+    public async Task<Unit> Handle(
+      UpdateCruisePackageCommand request,
+      CancellationToken cancellationToken
+    )
+    {
+      var cruisePackageEntity = await _iApplicationDbContext.CruisePackages.FindAsync(request.Id);
+
+      if (cruisePackageEntity == null)
+      {
+        throw new NotFoundException(nameof(CruisePackage), request.Id);
+      }
+
+      cruisePackageEntity.Name = request.Name;
+
+      await _iApplicationDbContext.SaveChangesAsync(cancellationToken);
+
+      return Unit.Value;
+    }
   }
 }
