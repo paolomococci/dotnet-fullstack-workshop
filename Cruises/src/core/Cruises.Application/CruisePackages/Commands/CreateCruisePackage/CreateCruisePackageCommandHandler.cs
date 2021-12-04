538d605a25b0d3017e0a823dba4ad5ff5ada719f
paolomococci <paolomococci@gmail.com>
2021-12-04 06:21:20 +0100

    class CreateCruisePackageCommandHandler belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Application/CruisePackages/Commands/CreateCruisePackage/CreateCruisePackageCommandHandler.cs
+++ Cruises/src/core/Cruises.Application/CruisePackages/Commands/CreateCruisePackage/CreateCruisePackageCommandHandler.cs
@@ -1,7 +1,46 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Cruises.Application.Common.Interfaces;
+using Cruises.Domain.Entities;
+using MediatR;
+
 namespace Cruises.Application.CruisePackages.Commands.CreateCruisePackage
 {
   public class CreateCruisePackageCommandHandler
+    : IRequestHandler<CreateCruisePackageCommand, int>
   {
 
+    private readonly IApplicationDbContext _iApplicationDbContext;
+
+    public CreateCruisePackageCommandHandler(
+      IApplicationDbContext context
+    )
+    {
+      _iApplicationDbContext = context;
+    }
+
+    public async Task<int> Handle(
+      CreateCruisePackageCommand request,
+      CancellationToken cancellationToken
+    )
+    {
+      var cruisePackageEntity = new CruisePackage
+      {
+        ListId = request.ListId,
+        Name = request.Name,
+        Hope = request.Hope,
+        MapLocation = request.MapLocation,
+        Price = request.Price,
+        Duration = request.Duration,
+        Confirmation = request.Confirmation,
+        Currency = request.Currency
+      };
+
+      _iApplicationDbContext.CruisePackages.Add(cruisePackageEntity);
+
+      await _iApplicationDbContext.SaveChangesAsync(cancellationToken);
+
+      return cruisePackageEntity.Id;
+    }
   }
 }
