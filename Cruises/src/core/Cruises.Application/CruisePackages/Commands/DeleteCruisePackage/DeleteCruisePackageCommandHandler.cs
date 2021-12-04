ffae485053907995de546a1d7439bd6548444ba9
paolomococci <paolomococci@gmail.com>
2021-12-04 06:13:17 +0100

    class DeleteCruisePackageCommandHandler belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Application/CruisePackages/Commands/DeleteCruisePackage/DeleteCruisePackageCommandHandler.cs
+++ Cruises/src/core/Cruises.Application/CruisePackages/Commands/DeleteCruisePackage/DeleteCruisePackageCommandHandler.cs
@@ -1,7 +1,42 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Cruises.Application.Common.Exceptions;
+using Cruises.Application.Common.Interfaces;
+using Cruises.Domain.Entities;
+using MediatR;
+
 namespace Cruises.Application.CruisePackages.Commands.DeleteCruisePackage
 {
   public class DeleteCruisePackageCommandHandler
+    : IRequestHandler<DeleteCruisePackageCommand>
   {
+    private readonly IApplicationDbContext _iApplicationDbContext;
 
+    public DeleteCruisePackageCommandHandler(
+      IApplicationDbContext context
+    )
+    {
+      _iApplicationDbContext = context;
+    }
+
+    public async Task<Unit> Handle(
+      DeleteCruisePackageCommand request,
+      CancellationToken cancellationToken
+    )
+    {
+      var cruisePackageEntity = await _iApplicationDbContext.CruisePackages
+        .FindAsync(request.Id);
+
+      if (cruisePackageEntity == null)
+      {
+        throw new NotFoundException(nameof(CruisePackage), request.Id);
+      }
+
+      _iApplicationDbContext.CruisePackages.Remove(cruisePackageEntity);
+
+      await _iApplicationDbContext.SaveChangesAsync(cancellationToken);
+
+      return Unit.Value;
+    }
   }
 }
