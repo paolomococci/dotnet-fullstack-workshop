c6ccb6408fa48fa07b1c7c4286e9338e1542a2d1
paolomococci <paolomococci@gmail.com>
2021-12-04 06:53:06 +0100

    class GetCruisesQueryHandler belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Application/CruiseLists/Queries/GetCruises/GetCruisesQueryHandler.cs
+++ Cruises/src/core/Cruises.Application/CruiseLists/Queries/GetCruises/GetCruisesQueryHandler.cs
@@ -1,7 +1,43 @@
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Cruises.Application.Common.Interfaces;
+using Cruises.Application.Dtos.Cruise;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
 namespace Cruises.Application.CruiseLists.Queries.GetCruises
 {
   public class GetCruisesQueryHandler
+    : IRequestHandler<GetCruisesQuery, CruisesVm>
   {
+    private readonly IApplicationDbContext _iApplicationDbContext;
+    private readonly IMapper _iMapper;
 
+    public GetCruisesQueryHandler(
+      IApplicationDbContext context,
+      IMapper mapper
+    )
+    {
+      _iApplicationDbContext = context;
+      _iMapper = mapper;
+    }
+
+    public async Task<CruisesVm> Handle(
+      GetCruisesQuery request,
+      CancellationToken cancellationToken
+    )
+    {
+      return new CruisesVm
+      {
+        Lists = await _iApplicationDbContext
+          .CruiseLists
+            .ProjectTo<CruiseListDto>(_iMapper.ConfigurationProvider)
+            .OrderBy(cruiseListDto => cruiseListDto.City)
+            .ToListAsync(cancellationToken)
+      };
+    }
   }
 }
