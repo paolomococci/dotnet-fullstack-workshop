e951659b2269295243e2be83914aa5fa2415adbb
paolomococci <paolomococci@gmail.com>
2021-12-04 20:13:34 +0100

    class Startup belonging to project Cruises.WebApi has been edited

--- Cruises/src/presentation/Cruises.WebApi/Startup.cs
+++ Cruises/src/presentation/Cruises.WebApi/Startup.cs
@@ -1,59 +1,136 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
+
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+using System.Collections.Generic;
+
+using Cruises.Application;
+using Cruises.Data;
+using Cruises.Identity.Helpers;
+using Cruises.Shared;
+using Cruises.WebApi.Filters;
+using Cruises.WebApi.Helpers;
 
 namespace Cruises.WebApi
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Cruises.WebApi", Version = "v1" });
-            });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Cruises.WebApi v1"));
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddApplication();
+      services.AddInfrastructureData();
+      services.AddInfrastructureShared(Configuration);
+      services.AddHttpContextAccessor();
+      services.AddControllers();
+
+      services.AddMemoryCache();
+
+      services.AddControllersWithViews(
+        options => options.Filters.Add(new ApiExceptionFilter())
+      );
+
+      services.Configure<ApiBehaviorOptions>(
+        options => options.SuppressModelStateInvalidFilter = true
+      );
+
+      services.AddControllers();
+
+      services.AddSwaggerGen(swaggerUIOptions =>
+      {
+        swaggerUIOptions.OperationFilter<SwaggerDefaultValues>();
+        swaggerUIOptions.AddSecurityDefinition(
+          "Bearer",
+            new OpenApiSecurityScheme
+            {
+              Description = "JWT Authorization header using the Bearer scheme.",
+              Name = "Authorization",
+              In = ParameterLocation.Header,
+              Type = SecuritySchemeType.Http,
+              Scheme = "bearer"
+            }
+        );
+        swaggerUIOptions.AddSecurityRequirement(
+          new OpenApiSecurityRequirement
+          {
+            {
+              new OpenApiSecurityScheme
+              {
+                Reference = new OpenApiReference
+                {
+                  Type = ReferenceType.SecurityScheme,
+                  Id = "Bearer"
+                }
+              }, new List<string>()
+            }
+          }
+        );
+      });
+
+      services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+
+      services.AddApiVersioning(config =>
+      {
+        config.DefaultApiVersion = new ApiVersion(1, 0);
+        config.AssumeDefaultVersionWhenUnspecified = true;
+        config.ReportApiVersions = true;
+      });
+
+      services.AddVersionedApiExplorer(options =>
+      {
+        options.GroupNameFormat = "'v'VVV";
+        options.SubstituteApiVersionInUrl = true;
+      });
+    }
+
+    public void Configure(
+      IApplicationBuilder app,
+      IWebHostEnvironment env,
+      IApiVersionDescriptionProvider provider
+    )
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(swaggerUIOptions =>
+        {
+          foreach (var description in provider.ApiVersionDescriptions)
+          {
+            swaggerUIOptions.SwaggerEndpoint(
+              $"/swagger/{description.GroupName}/swagger.json",
+              description.GroupName.ToUpperInvariant()
+            );
+          }
+        });
+      }
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseMiddleware<JwtMiddleware>();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
