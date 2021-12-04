18d8ccfe4b10bb173a8f219b1871893f9e6b9cf8
paolomococci <paolomococci@gmail.com>
2021-12-04 06:08:36 +0100

    class UpdateCruisePackageCommandValidator belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Application/CruisePackages/Commands/UpdateCruisePackage/UpdateCruisePackageCommandValidator.cs
+++ Cruises/src/core/Cruises.Application/CruisePackages/Commands/UpdateCruisePackage/UpdateCruisePackageCommandValidator.cs
@@ -1,7 +1,35 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Cruises.Application.Common.Interfaces;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
 namespace Cruises.Application.CruisePackages.Commands.UpdateCruisePackage
 {
   public class UpdateCruisePackageCommandValidator
+    : AbstractValidator<UpdateCruisePackageCommand>
   {
+    private readonly IApplicationDbContext _iApplicationDbContext;
 
+    public UpdateCruisePackageCommandValidator(
+      IApplicationDbContext context
+    )
+    {
+      _iApplicationDbContext = context;
+
+      RuleFor(updateCruisePackageCommand => updateCruisePackageCommand.Name)
+          .NotEmpty().WithMessage("Name field is required.")
+          .MaximumLength(128).WithMessage("Name field must not exceed 128 characters.")
+          .MustAsync(NameFieldMustBeUnique).WithMessage("The specified name field already used.");
+    }
+
+    public async Task<bool> NameFieldMustBeUnique(
+      string name, CancellationToken cancellationToken
+    )
+    {
+      return await _iApplicationDbContext.CruisePackages.AllAsync(
+        cruisePackage => cruisePackage.Name != name
+      );
+    }
   }
 }
