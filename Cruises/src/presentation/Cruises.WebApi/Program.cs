bbfa7bb66a3587fd8a2c55dfa9265c2302dc8f9b
paolomococci <paolomococci@gmail.com>
2021-12-04 20:04:09 +0100

    class Program belonging to project Cruises.WebApi has been edited

--- Cruises/src/presentation/Cruises.WebApi/Program.cs
+++ Cruises/src/presentation/Cruises.WebApi/Program.cs
@@ -1,26 +1,63 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Exceptions;
+using Serilog.Formatting.Compact;
 
 namespace Cruises.WebApi
 {
-    public class Program
+  public class Program
+  {
+    public static int Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+      var name = Assembly.GetExecutingAssembly().GetName();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+      Log.Logger = new LoggerConfiguration()
+          .MinimumLevel.Debug()
+          .Enrich.FromLogContext()
+          .Enrich.WithExceptionDetails()
+          .Enrich.WithMachineName()
+          .Enrich.WithProperty("Assembly", $"{name.Name}")
+          .Enrich.WithProperty("Assembly", $"{name.Version}")
+          .WriteTo.SQLite(
+              Environment.CurrentDirectory + @"/Logs/log.db",
+              restrictedToMinimumLevel: LogEventLevel.Information,
+              storeTimestampInUtc: true
+          )
+          .WriteTo.File(
+              new CompactJsonFormatter(),
+              Environment.CurrentDirectory + @"/Logs/log.json",
+              rollingInterval: RollingInterval.Hour,
+              restrictedToMinimumLevel: LogEventLevel.Debug
+          )
+          .WriteTo.Console().CreateLogger();
+
+      try
+      {
+        Log.Information("starting host...");
+        CreateHostBuilder(args).Build().Run();
+        return 0;
+      }
+      catch (System.Exception ex)
+      {
+        Log.Fatal(ex, "... terminated unexpectedly!");
+        return 1;
+      }
+      finally
+      {
+        Log.CloseAndFlush();
+      }
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .UseSerilog()
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+              webBuilder.UseStartup<Startup>();
+            });
+  }
 }
