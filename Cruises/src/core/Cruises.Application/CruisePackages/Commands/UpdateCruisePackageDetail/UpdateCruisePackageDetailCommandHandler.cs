5c734bea2faf91fc8d6b3cb5ae6573a392977b18
paolomococci <paolomococci@gmail.com>
2021-12-04 05:57:58 +0100

    class UpdateCruisePackageDetailCommandHandler belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Application/CruisePackages/Commands/UpdateCruisePackageDetail/UpdateCruisePackageDetailCommandHandler.cs
+++ Cruises/src/core/Cruises.Application/CruisePackages/Commands/UpdateCruisePackageDetail/UpdateCruisePackageDetailCommandHandler.cs
@@ -1,7 +1,52 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Cruises.Application.Common.Exceptions;
+using Cruises.Application.Common.Interfaces;
+using Cruises.Domain.Entities;
+using MediatR;
+
 namespace Cruises.Application.CruisePackages.Commands.UpdateCruisePackageDetail
 {
   public class UpdateCruisePackageDetailCommandHandler
+    : IRequestHandler<UpdateCruisePackageDetailCommand>
   {
+    private readonly IApplicationDbContext _iApplicationDbContext;
 
+    public UpdateCruisePackageDetailCommandHandler(
+      IApplicationDbContext context
+    )
+    {
+      _iApplicationDbContext = context;
+    }
+
+    public async Task<Unit> Handle(
+      UpdateCruisePackageDetailCommand request,
+      CancellationToken cancellationToken
+    )
+    {
+      var entity = await _iApplicationDbContext.CruisePackages.FindAsync(request.Id);
+
+      if (entity == null)
+      {
+        throw new NotFoundException(
+            nameof(CruisePackage),
+            request.Id
+        );
+      }
+
+      entity.Id = request.Id;
+      entity.ListId = request.ListId;
+      entity.Name = request.Name;
+      entity.Hope = request.Hope;
+      entity.MapLocation = request.MapLocation;
+      entity.Price = request.Price;
+      entity.Duration = request.Duration;
+      entity.Confirmation = request.Confirmation;
+      entity.Currency = request.Currency;
+
+      await _iApplicationDbContext.SaveChangesAsync(cancellationToken);
+
+      return Unit.Value;
+    }
   }
 }
