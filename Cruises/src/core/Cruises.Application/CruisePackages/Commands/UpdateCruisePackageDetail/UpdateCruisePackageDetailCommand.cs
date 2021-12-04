3b6f896d2425a5eb03d4fc0ed6eee37463d5d51a
paolomococci <paolomococci@gmail.com>
2021-12-04 06:00:00 +0100

    class UpdateCruisePackageDetailCommand belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Application/CruisePackages/Commands/UpdateCruisePackageDetail/UpdateCruisePackageDetailCommand.cs
+++ Cruises/src/core/Cruises.Application/CruisePackages/Commands/UpdateCruisePackageDetail/UpdateCruisePackageDetailCommand.cs
@@ -1,7 +1,19 @@
+using Cruises.Domain.Enums;
+using MediatR;
+
 namespace Cruises.Application.CruisePackages.Commands.UpdateCruisePackageDetail
 {
   public class UpdateCruisePackageDetailCommand
+    : IRequest
   {
-
+    public int Id { get; set; }
+    public int ListId { get; set; }
+    public string Name { get; set; }
+    public string Hope { get; set; }
+    public string MapLocation { get; set; }
+    public float Price { get; set; }
+    public int Duration { get; set; }
+    public bool Confirmation { get; set; }
+    public Currency Currency { get; set; }
   }
 }
