8bef501380c9d0be58ca87ba328df3c8e922eab5
paolomococci <paolomococci@gmail.com>
2021-12-04 18:33:44 +0100

    class DependencyInjection belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Application/DependencyInjection.cs
+++ Cruises/src/core/Cruises.Application/DependencyInjection.cs
@@ -1,7 +1,25 @@
+using System.Reflection;
+using Cruises.Application.Common.Behaviors;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Cruises.Application
 {
-	public class DependencyInjection
-	{
-		
-	}
+  public static class DependencyInjection
+  {
+    public static IServiceCollection AddApplication(
+        this IServiceCollection services
+    )
+    {
+      services.AddAutoMapper(Assembly.GetExecutingAssembly());
+      services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+      services.AddMediatR(Assembly.GetExecutingAssembly());
+      services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehavior<,>));
+      services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+      services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehavior<,>));
+
+      return services;
+    }
+  }
 }
