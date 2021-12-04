f45743cf4a02c0972749b8fa5bbc69d904b382e1
paolomococci <paolomococci@gmail.com>
2021-12-04 19:47:40 +0100

    class DateTimeService belonging to project Cruises.Shared has been edited

--- Cruises/src/infrastructure/Cruises.Shared/Services/DateTimeService.cs
+++ Cruises/src/infrastructure/Cruises.Shared/Services/DateTimeService.cs
@@ -1,7 +1,11 @@
+using System;
+using Cruises.Application.Common.Interfaces;
+
 namespace Cruises.Shared.Services
 {
-	public class DateTimeService
-	{
-		
-	}
+  public class DateTimeService
+    : IDateTime
+  {
+    public DateTime NowUtc => DateTime.UtcNow;
+  }
 }
