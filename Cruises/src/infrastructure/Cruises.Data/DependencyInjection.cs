e3da588f6ba682ee5c3bad044ef7ffdac9e393f7
paolomococci <paolomococci@gmail.com>
2021-12-04 18:47:14 +0100

    class DependencyInjection belonging to project Cruises.Data has been edited

--- Cruises/src/infrastructure/Cruises.Data/DependencyInjection.cs
+++ Cruises/src/infrastructure/Cruises.Data/DependencyInjection.cs
@@ -1,7 +1,28 @@
+using Cruises.Application.Common.Interfaces;
+using Cruises.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Cruises.Data
 {
-	public class DependencyInjection
-	{
-		
-	}
+  public static class DependencyInjection
+  {
+    public static IServiceCollection AddInfrastructureData(
+        this IServiceCollection services
+    )
+    {
+      services.AddDbContext<ApplicationDbContext>(
+          options => options.UseSqlite(
+            "Data Source=CruisesDatabase.sqlite3"
+          )
+      );
+
+      services.AddScoped<IApplicationDbContext>(
+          provider => (IApplicationDbContext)provider
+            .GetService<ApplicationDbContext>()
+      );
+
+      return services;
+    }
+  }
 }
