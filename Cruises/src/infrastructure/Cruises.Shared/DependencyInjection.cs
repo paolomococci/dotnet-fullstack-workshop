fdb755c9cc441083856b5de0237690b4a1b5040f
paolomococci <paolomococci@gmail.com>
2021-12-04 19:52:28 +0100

    class DependencyInjection belonging to project Cruises.Shared has been edited

--- Cruises/src/infrastructure/Cruises.Shared/DependencyInjection.cs
+++ Cruises/src/infrastructure/Cruises.Shared/DependencyInjection.cs
@@ -1,7 +1,25 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Cruises.Application.Common.Interfaces;
+using Cruises.Domain.Settings;
+using Cruises.Shared.Files;
+using Cruises.Shared.Services;
+
 namespace Cruises.Shared
 {
-	public class DependencyInjection
-	{
-		
-	}
+  public static class DependencyInjection
+  {
+    public static IServiceCollection AddInfrastructureShared(
+      this IServiceCollection services,
+      IConfiguration config
+    )
+    {
+      services.Configure<MailSettings>(config.GetSection("MailSettings"));
+      services.AddTransient<IDateTime, DateTimeService>();
+      services.AddTransient<IEmailService, EmailService>();
+      services.AddTransient<ICsvFileBuilder, CsvFileBuilder>();
+
+      return services;
+    }
+  }
 }
