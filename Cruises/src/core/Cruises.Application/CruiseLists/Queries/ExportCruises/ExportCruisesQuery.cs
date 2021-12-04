914ac424776ca7e61dad435a5ce62e7321b0f5c7
paolomococci <paolomococci@gmail.com>
2021-12-04 11:22:33 +0100

    class ExportCruisesQuery belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Application/CruiseLists/Queries/ExportCruises/ExportCruisesQuery.cs
+++ Cruises/src/core/Cruises.Application/CruiseLists/Queries/ExportCruises/ExportCruisesQuery.cs
@@ -1,7 +1,10 @@
+using MediatR;
+
 namespace Cruises.Application.CruiseLists.Queries.ExportCruises
 {
   public class ExportCruisesQuery
+    : IRequest<ExportCruisesVm>
   {
-
+    public int ListId { get; set; }
   }
 }
