5ecd7900b2b4f9d32045113d94a549b78062c669
paolomococci <paolomococci@gmail.com>
2021-12-04 06:01:44 +0100

    class UpdateCruisePackageCommand belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Application/CruisePackages/Commands/UpdateCruisePackage/UpdateCruisePackageCommand.cs
+++ Cruises/src/core/Cruises.Application/CruisePackages/Commands/UpdateCruisePackage/UpdateCruisePackageCommand.cs
@@ -1,7 +1,11 @@
+using MediatR;
+
 namespace Cruises.Application.CruisePackages.Commands.UpdateCruisePackage
 {
   public class UpdateCruisePackageCommand
+    : IRequest
   {
-
+    public int Id { get; set; }
+    public string Name { get; set; }
   }
 }
