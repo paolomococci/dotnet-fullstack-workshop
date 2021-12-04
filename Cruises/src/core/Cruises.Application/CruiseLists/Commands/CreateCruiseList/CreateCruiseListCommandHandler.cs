4d7a35bf6f42517358aa029d5f7f250b50086c91
paolomococci <paolomococci@gmail.com>
2021-12-04 11:30:41 +0100

    class CreateCruiseListCommandHandler belonging to project Cruises.Application has been edited

--- Cruises/src/core/Cruises.Application/CruiseLists/Commands/CreateCruiseList/CreateCruiseListCommandHandler.cs
+++ Cruises/src/core/Cruises.Application/CruiseLists/Commands/CreateCruiseList/CreateCruiseListCommandHandler.cs
@@ -1,7 +1,36 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Cruises.Application.Common.Interfaces;
+using Cruises.Domain.Entities;
+using MediatR;
+
 namespace Cruises.Application.CruiseLists.Commands.CreateCruiseList
 {
   public class CreateCruiseListCommandHandler
+    : IRequestHandler<CreateCruiseListCommand, int>
   {
+    private readonly IApplicationDbContext _iApplicationDbContext;
 
+    public CreateCruiseListCommandHandler(IApplicationDbContext context)
+    {
+      _iApplicationDbContext = context;
+    }
+
+    public async Task<int> Handle(
+      CreateCruiseListCommand request,
+      CancellationToken cancellationToken
+    )
+    {
+      var cruiseListEntity = new CruiseList
+      {
+        City = request.City
+      };
+
+      _iApplicationDbContext.CruiseLists.Add(cruiseListEntity);
+
+      await _iApplicationDbContext.SaveChangesAsync(cancellationToken);
+
+      return cruiseListEntity.Id;
+    }
   }
 }
