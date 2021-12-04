09a6af770b8a87e27525251a9eeeb032218cc5f6
paolomococci <paolomococci@gmail.com>
2021-12-04 18:43:44 +0100

    class CruisesDbContext belonging to project Cruises.Data has been edited

--- Cruises/src/infrastructure/Cruises.Data/Contexts/CruisesDbContext.cs
+++ Cruises/src/infrastructure/Cruises.Data/Contexts/CruisesDbContext.cs
@@ -1,7 +1,16 @@
+using Cruises.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace Cruises.Data.Contexts
 {
   public class CruisesDbContext
+    : DbContext
   {
-
+    public CruisesDbContext(
+      DbContextOptions<CruisesDbContext> dbContextOptions
+    ) : base(dbContextOptions)
+    { }
+    public DbSet<CruiseList> CruiseLists { get; set; }
+    public DbSet<CruisePackage> CruisePackages { get; set; }
   }
 }
