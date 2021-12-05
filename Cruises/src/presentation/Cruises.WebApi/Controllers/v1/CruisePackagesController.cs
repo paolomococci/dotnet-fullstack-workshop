efcc6f3fb50e5929f33ecd9b6b98e57b54b1ee50
paolomococci <paolomococci@gmail.com>
2021-12-05 06:17:07 +0100

    class CruisePackagesController belonging to project Cruises.WebApi has been edited

--- Cruises/src/presentation/Cruises.WebApi/Controllers/v1/CruisePackagesController.cs
+++ Cruises/src/presentation/Cruises.WebApi/Controllers/v1/CruisePackagesController.cs
@@ -1,7 +1,67 @@
+using System.Threading.Tasks;
+using Cruises.Application.CruisePackages.Commands.CreateCruisePackage;
+using Cruises.Application.CruisePackages.Commands.DeleteCruisePackage;
+using Cruises.Application.CruisePackages.Commands.UpdateCruisePackage;
+using Cruises.Application.CruisePackages.Commands.UpdateCruisePackageDetail;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Cruises.WebApi.Controllers.v1
 {
   public class CruisePackagesController
+    : ApiController
   {
+    [HttpPost]
+    public async Task<ActionResult<int>> Create(
+      CreateCruisePackageCommand createCruisePackageCommand
+    )
+    {
+      return await Mediator.Send(createCruisePackageCommand);
+    }
 
+    [HttpPut("{id}")]
+    public async Task<ActionResult> Update(
+      int id,
+      UpdateCruisePackageCommand updateCruisePackageCommand
+    )
+    {
+      if (id != updateCruisePackageCommand.Id)
+      {
+        return BadRequest();
+      }
+
+      await Mediator.Send(updateCruisePackageCommand);
+
+      return StatusCode(StatusCodes.Status205ResetContent);
+    }
+
+    [HttpPut("[action]")]
+    public async Task<ActionResult> UpdateItemDetails(
+      int id,
+      UpdateCruisePackageDetailCommand updateCruisePackageDetailCommand
+    )
+    {
+      if (id != updateCruisePackageDetailCommand.Id)
+      {
+        return StatusCode(StatusCodes.Status205ResetContent);
+      }
+
+      await Mediator.Send(updateCruisePackageDetailCommand);
+
+      return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Delete(int id)
+    {
+      await Mediator.Send(
+        new DeleteCruisePackageCommand
+        {
+          Id = id
+        }
+      );
+
+      return NoContent();
+    }
   }
 }
