74aca3fb3abafd33132d44639a4c5bf08c672afe
paolomococci <paolomococci@gmail.com>
2021-12-05 06:11:29 +0100

    class ApiController belonging to project Cruises.WebApi has been edited

--- Cruises/src/presentation/Cruises.WebApi/Controllers/v1/ApiController.cs
+++ Cruises/src/presentation/Cruises.WebApi/Controllers/v1/ApiController.cs
@@ -1,7 +1,20 @@
+using Cruises.Identity.Helpers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Cruises.WebApi.Controllers.v1
 {
-  public class ApiController
+  [Authorize]
+  [ApiVersion("1.0")]
+  [ApiController]
+  [Route("api/v{version:apiVersion}/[controller]")]
+  public abstract class ApiController
+    : ControllerBase
   {
+    private IMediator _iMediator;
 
+    protected IMediator Mediator => _iMediator ??= HttpContext
+        .RequestServices.GetService<IMediator>();
   }
 }
