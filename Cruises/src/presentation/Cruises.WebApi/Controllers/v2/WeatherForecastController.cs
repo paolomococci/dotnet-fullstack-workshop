e5a84a2792e7c14096792776e6b8c5f5282438d5
paolomococci <paolomococci@gmail.com>
2021-12-05 06:06:41 +0100

    class WeatherForecastController belonging to project Cruises.WebApi has been edited

--- Cruises/src/presentation/Cruises.WebApi/Controllers/v2/WeatherForecastController.cs
+++ Cruises/src/presentation/Cruises.WebApi/Controllers/v2/WeatherForecastController.cs
@@ -1,7 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
 namespace Cruises.WebApi.Controllers.v2
 {
+  [ApiVersion("2.0")]
+  [ApiController]
+  [Route("api/v{version:apiVersion}/[controller]")]
   public class WeatherForecastController
+    : ControllerBase
   {
+    private static readonly string[] Summaries = new[]
+    {
+      "Freezing",
+      "Bracing",
+      "Chilly",
+      "Cool",
+      "Mild",
+      "Warm",
+      "Balmy",
+      "Hot",
+      "Sweltering",
+      "Scorching"
+    };
 
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(
+      ILogger<WeatherForecastController> logger
+    )
+    {
+      _logger = logger;
+    }
+
+    [HttpPost]
+    public IEnumerable<WeatherForecast> Post(string place)
+    {
+      var rng = new Random();
+      return Enumerable.Range(1, 5)
+          .Select(index => new WeatherForecast
+          {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = rng.Next(-31, 53),
+            Summary = Summaries[rng.Next(Summaries.Length)],
+            Place = place
+          })
+          .ToArray();
+    }
   }
 }
