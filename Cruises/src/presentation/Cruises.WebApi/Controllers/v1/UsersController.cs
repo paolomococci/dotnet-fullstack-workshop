f4249533a68a62b3e9e52b9a104646f4f7335863
paolomococci <paolomococci@gmail.com>
2021-12-05 06:12:54 +0100

    class UsersController belonging to project Cruises.WebApi has been edited

--- Cruises/src/presentation/Cruises.WebApi/Controllers/v1/UsersController.cs
+++ Cruises/src/presentation/Cruises.WebApi/Controllers/v1/UsersController.cs
@@ -1,7 +1,38 @@
+using Cruises.Application.Common.Interfaces;
+using Cruises.Application.Dtos.User;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Cruises.WebApi.Controllers.v1
 {
+  [ApiVersion("1.0")]
+  [ApiController]
+  [Route("api/v{version:apiVersion}/[controller]")]
   public class UsersController
+    : ControllerBase
   {
+    private readonly IUserService _iUserService;
+    public UsersController(
+      IUserService userService
+    ) => _iUserService = userService;
 
+    [HttpPost("auth")]
+    public IActionResult Authenticate(
+      [FromBody] AuthenticateRequest model
+    )
+    {
+      var response = _iUserService.Authenticate(model);
+
+      if (response == null)
+      {
+        return BadRequest(
+          new
+          {
+            message = "access denied"
+          }
+        );
+      }
+
+      return Ok(response);
+    }
   }
 }
